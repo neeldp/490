15752e54e9e32446538a00c24c5d5783b0a0aaea
unknown <brendanaszklar@gmail.com>
2021-09-22 17:11:10 -0400

    spacing

--- db_key.php
+++ db_key.php
@@ -7,7 +7,7 @@
 		global $sql_host, $sql_username, $sql_password, $sql_database;
 		$conn=new mysqli($sql_host,$sql_username,$sql_password);
 		if(mysqli_connect_error() !== null){
-			return false;
+			 return false;
 		}
 		$conn -> select_db($sql_database);
 		return $conn;
