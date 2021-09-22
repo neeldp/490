93b198de001cfb557b00e750b372e39209f05925
neeldp <ndp69@njit.edu>
2021-09-22 17:35:16 -0400

    Update db_key.php

--- db_key.php
+++ db_key.php
@@ -1,9 +1,9 @@
 <?php
 	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$cleardb_server = $cleardb_url["host"];
-	$cleardb_username = $cleardb_url["user"];
-	$cleardb_password = $cleardb_url["pass"];
-	$cleardb_db = substr($cleardb_url["path"], 1);
+	$sql_host = $cleardb_url["host"];
+	$sql_username = $cleardb_url["user"];
+	$sql_password = $cleardb_url["pass"];
+	$sql_database = substr($cleardb_url["path"], 1);
 
 	#$sql_host="localhost";
 	#$sql_username="root";
