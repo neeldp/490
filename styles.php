8d401bbd8b7272000341f7fea8847a689c3315fa
brendan123 <brendanaszklar@gmail.com>
2021-10-03 14:44:32 -0400

    update

--- styles.php
+++ styles.php
@@ -102,3 +102,28 @@ button.ghost {
 	z-index: 5;
 	animation: show 0.6s;
 }
+.overlay {
+	background: #FF416C;
+	background: -webkit-linear-gradient(to right, #FF4B2B, #FF416C);
+	background: linear-gradient(to right, #FF4B2B, #FF416C);
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: 0 0;
+	color: #FFFFFF;
+	position: relative;
+	left: -100%;
+	height: 100%;
+	width: 200%;
+  	transform: translateX(0);
+	transition: transform 0.6s ease-in-out;
+}
+.overlay-container {
+	position: absolute;
+	top: 0;
+	left: 50%;
+	width: 50%;
+	height: 100%;
+	overflow: hidden;
+	transition: transform 0.6s ease-in-out;
+	z-index: 100;
+}
