8466b431d125ea16bbbd480d18ff70fb840a5416
brendan123 <brendanaszklar@gmail.com>
2021-10-03 14:05:23 -0400

    add styles.php

--- index.php
+++ index.php
@@ -3,23 +3,19 @@ require 'db_key.php';
 ?>
 <!DOCTYPE html>
 <!-- This file is used for the login screen of the CS 490 SEC. 003 Group 8 Project -->
-<html lang = "en">
-	<head>
-        	<title> Group 8 Login Screen </title>
-       		<meta charset = "utf-8" />
-	</head>
-    
+<html lang = "en">    
 	<body>
        	<h1> <b> Group 8 Login Screen </h1>
 
-		
-		<form method = 'POST' action = 'backend.php' >
+	<form method = 'POST' action = 'backend.php' >
 		<div class="form-group">
-		<label>Username : </label>
-		<input class= 'form-control w-25' type="text" name="username"><br><br>
-		<label>Password :</label>
-		<input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
-		</div><button class = 'btn btn-outline-info' type="submit" name="login" value= 'login' class="submit">Login</button></form>
-		
-    	</body>
+			<label>Username : </label>
+			<input class= 'form-control w-25' type="text" name="username"><br><br>
+
+			<label>Password :</label>
+			<input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
+		</div> 
+		<button class = 'btn btn-outline-info' type="submit" name="login" value= 'login' class="submit">Login</button>
+	</form>	
+    </body>
 </html>
