d085825c5b796a0a41bbee41a0fb2fc1de537c56
brendan123 <brendanaszklar@gmail.com>
2021-10-03 14:24:38 -0400

    updateStyles.php

--- index.php
+++ index.php
@@ -5,18 +5,20 @@ require 'db_key.php';
 <!-- This file is used for the login screen of the CS 490 SEC. 003 Group 8 Project -->
 <html lang = "en">    
 	<body>
-       	<h1> <b> Group 8 Login Screen </h1>
+		<div class="containter" id="containter"> 
+			<div class ="form-container sign-in">
+				<form method = 'POST' action = 'backend.php' >
+					<h1> Sign in to !Twitter </h1> 
+					<div class="form-group">
+						<label>Username : </label>
+						<input class= 'form-control w-25' type="text" name="username"><br><br>
 
-	<form method = 'POST' action = 'backend.php' >
-		<h1> Sign in </h1> 
-		<div class="form-group">
-			<label>Username : </label>
-			<input class= 'form-control w-25' type="text" name="username"><br><br>
-
-			<label>Password :</label>
-			<input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
-		</div> 
-		<button class = 'btn btn-outline-info' type="submit" name="login" value= 'login' class="submit">Login</button>
-	</form>	
+						<label>Password :</label>
+						<input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
+					</div> 
+					<button class = 'btn btn-outline-info' type="submit" name="login" value= 'login' class="submit">Login</button>
+				</form>	
+			</div>
+		</div>
     </body>
 </html>
--- styles.php
+++ styles.php
@@ -29,3 +29,52 @@ input {
 	width: 100%;
 }
 
+h1 {
+	font-weight: bold;
+	margin: 0;
+}
+
+button {
+	border-radius: 20px;
+	border: 1px solid #FF4B2B;
+	background-color: #FF4B2B;
+	color: #FFFFFF;
+	font-size: 12px;
+	font-weight: bold;
+	padding: 12px 45px;
+	letter-spacing: 1px;
+	text-transform: uppercase;
+	transition: transform 80ms ease-in;
+}
+
+button:active {
+	transform: scale(0.95);
+}
+
+button:focus {
+	outline: none;
+}
+
+button.ghost {
+	background-color: transparent;
+	border-color: #FFFFFF;
+}
+
+.container {
+	background-color: #fff;
+	border-radius: 10px;
+  	box-shadow: 0 14px 28px rgba(0,0,0,0.25), 
+			0 10px 10px rgba(0,0,0,0.22);
+	position: relative;
+	overflow: hidden;
+	width: 768px;
+	max-width: 100%;
+	min-height: 480px;
+}
+
+.form-container {
+	position: absolute;
+	top: 0;
+	height: 100%;
+	transition: all 0.6s ease-in-out;
+}
