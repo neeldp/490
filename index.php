ad3980bf2d43209f54aa39c26a93150771f56701
brendan123 <brendanaszklar@gmail.com>
2021-10-03 14:52:58 -0400

    update index

--- index.php
+++ index.php
@@ -6,21 +6,16 @@ require 'db_key.php';
 <html lang = "en">    
 	<body>
 		<div class="containter" id="containter"> 
-		<div class="form-container sign-up-container">
-		<form action="#">
-			<h1>Create Account</h1>
-			<div class="social-container">
-				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
-				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
-				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
+			<div class="form-container sign-up-container">
+				<form action="#">
+					<h1>Create Account</h1>
+
+					<span>Make a Username</span>
+					<input type="text" placeholder="Username" />
+					<input type="password" placeholder="Password" />
+					<button>Sign Up</button>
+				</form>
 			</div>
-			<span>or use your email for registration</span>
-			<input type="text" placeholder="Name" />
-			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Password" />
-			<button>Sign Up</button>
-		</form>
-	</div>
 
 				<div class ="form-container sign-in">
 					<form method = 'POST' action = 'backend.php' >
