2ece8039826d99d004aedbfe2303a7ce0d45566f
brendan123 <brendanaszklar@gmail.com>
2021-10-04 14:03:26 -0400

    Update styles.php

--- styles.php
+++ styles.php
@@ -5,21 +5,13 @@ $font_size = '0.7em';
 $border = '1px solid';
 ?>
 
+
 @import url('https://fonts.googleapis.com/css?family=Montserrat:400,800');
-*{
-    box-sizing:border-box;
+
+* {
+	box-sizing: border-box;
 }
 
-form {
-	background-color: #FFFFFF;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	flex-direction: column;
-	padding: 0 50px;
-	height: 100%;
-	text-align: center;
-}
 body {
 	background: #f6f5f7;
 	display: flex;
@@ -30,17 +22,33 @@ body {
 	height: 100vh;
 	margin: -20px 0 50px;
 }
-input {
-	background-color: #eee;
-	border: none;
-	padding: 12px 15px;
-	margin: 8px 0;
-	width: 100%;
-}
 
 h1 {
 	font-weight: bold;
 	margin: 0;
+}
+
+h2 {
+	text-align: center;
+}
+
+p {
+	font-size: 14px;
+	font-weight: 100;
+	line-height: 20px;
+	letter-spacing: 0.5px;
+	margin: 20px 0 30px;
+}
+
+span {
+	font-size: 12px;
+}
+
+a {
+	color: #333;
+	font-size: 14px;
+	text-decoration: none;
+	margin: 15px 0;
 }
 
 button {
@@ -69,6 +77,25 @@ button.ghost {
 	border-color: #FFFFFF;
 }
 
+form {
+	background-color: #FFFFFF;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+	padding: 0 50px;
+	height: 100%;
+	text-align: center;
+}
+
+input {
+	background-color: #eee;
+	border: none;
+	padding: 12px 15px;
+	margin: 8px 0;
+	width: 100%;
+}
+
 .container {
 	background-color: #fff;
 	border-radius: 10px;
@@ -93,18 +120,19 @@ button.ghost {
 	width: 50%;
 	z-index: 2;
 }
-.container.right-panel-active .sign-in {
+
+.container.right-panel-active .sign-in-container {
 	transform: translateX(100%);
 }
 
-.sign-up {
+.sign-up-container {
 	left: 0;
 	width: 50%;
 	opacity: 0;
 	z-index: 1;
 }
 
-.container.right-panel-active .sign-up {
+.container.right-panel-active .sign-up-container {
 	transform: translateX(100%);
 	opacity: 1;
 	z-index: 5;
@@ -123,6 +151,21 @@ button.ghost {
 	}
 }
 
+.overlay-container {
+	position: absolute;
+	top: 0;
+	left: 50%;
+	width: 50%;
+	height: 100%;
+	overflow: hidden;
+	transition: transform 0.6s ease-in-out;
+	z-index: 100;
+}
+
+.container.right-panel-active .overlay-container{
+	transform: translateX(-100%);
+}
+
 .overlay {
 	background: #FF416C;
 	background: -webkit-linear-gradient(to right, #FF4B2B, #FF416C);
@@ -138,16 +181,11 @@ button.ghost {
   	transform: translateX(0);
 	transition: transform 0.6s ease-in-out;
 }
-.overlay-container {
-	position: absolute;
-	top: 0;
-	left: 50%;
-	width: 50%;
-	height: 100%;
-	overflow: hidden;
-	transition: transform 0.6s ease-in-out;
-	z-index: 100;
+
+.container.right-panel-active .overlay {
+  	transform: translateX(50%);
 }
+
 .overlay-panel {
 	position: absolute;
 	display: flex;
@@ -178,4 +216,44 @@ button.ghost {
 
 .container.right-panel-active .overlay-right {
 	transform: translateX(20%);
+}
+
+.social-container {
+	margin: 20px 0;
+}
+
+.social-container a {
+	border: 1px solid #DDDDDD;
+	border-radius: 50%;
+	display: inline-flex;
+	justify-content: center;
+	align-items: center;
+	margin: 0 5px;
+	height: 40px;
+	width: 40px;
+}
+
+footer {
+    background-color: #222;
+    color: #fff;
+    font-size: 14px;
+    bottom: 0;
+    position: fixed;
+    left: 0;
+    right: 0;
+    text-align: center;
+    z-index: 999;
+}
+
+footer p {
+    margin: 10px 0;
+}
+
+footer i {
+    color: red;
+}
+
+footer a {
+    color: #3c97bf;
+    text-decoration: none;
 }
