0937cb5486b4fa441ade41b20084511657f28140
brendan123 <brendan123@users.noreply.github.com>
2021-10-11 18:52:17 -0400

    loginupdate

--- header.php
+++ header.php
@@ -4,5 +4,4 @@
     <title> Group 8 Login Screen </title>
     <meta charset = "utf-8" />
     <link rel="stylesheet" href="indexstyles.css" media="screen">
-    <script type = "text/javascript" src="flip.js"></script>
 </head>
--- index2.php
+++ index2.php
@@ -1,6 +1,4 @@
-<?php require 'header.php';
-
-?>
+<?php require 'header.php'; require 'db_key.php';?>
 
 <div class="scene">
     <div class="card">
@@ -34,11 +32,11 @@
                         <label>Password :</label><br>
                         <input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
                     </div> 
-                    <br><button class = 'button-login' type="submit" name="login" value= 'login' class="submit">Login</button>
+                    <br><button class = 'button-login' type="submit" name="register" value= 'login' class="register">Login</button>
                 </form>	
                 <div class="switch">
-                    <br><br><h2> No Account? </h2>
-                    <button class='button-register' >Sign-up</button>
+                    <br><br><h2> Have an account? </h2>
+                    <button class='button-register1' >Login</button>
                 </div>
             </div>
         </div>
@@ -46,6 +44,8 @@
     
     <script> 
         var card = document.querySelector('.card'); 
-         btn = document.querySelector('.button-register');
-        btn.addEventListener('click', function () { card.classList.toggle('is-flipped');}); </script>
+        var btn = document.querySelector('.button-register');
+        var btn1 = document.querySelector('.button-register1');
+        btn.addEventListener('click', function () { card.classList.toggle('is-flipped');});
+        btn1.addEventListener('click', function () { card.classList.toggle('is-flipped');}); </script>
 </div>
