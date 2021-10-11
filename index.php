ab787084da9910dc67586724513728b7116a2fdd
brendan123 <brendan123@users.noreply.github.com>
2021-10-11 18:57:18 -0400

    update

--- index.php
+++ index.php
@@ -1,47 +1,51 @@
-<?php require 'header.php';
-require 'db_key.php';
-?>
+<?php require 'header.php'; require 'db_key.php';?>
 
+<div class="scene">
+    <div class="card">
+        <div class="card__face card__face--front">
+            <div class = "form-container">
+                <form method = 'POST' action = 'backend.php' >
+                    <h1 class = "title"> Sign in to !Twitter </h1> <br>
+                    <div class="form-group">
+                        <label>Username : </label><br>
+                        <input class= 'form-control w-25' type="text" name="username"><br>
 
-<!-- This file is used for the login screen of the CS 490 SEC. 003 Group 8 Project -->
- 
-	<body>
-		<div class="containter" id="containter">
-			<div class="form-container sign-up">
-				<form action="#">
-					<h1>Create Account</h1>
-					<span>Make a Username</span>
-					<input type="text" placeholder="Username" />
-					<input type="password" placeholder="Password" />
-					<button>Sign Up</button>
-				</form>
-			</div>
-			<div class ="form-container sign-in">
-				<form method = 'POST' action = 'backend.php' >
-					<h1> Sign in to !Twitter </h1> 
-					<div class="form-group">
-						<label>Username : </label>
-						<input class= 'form-control w-25' type="text" name="username"><br><br>
-
-						<label>Password :</label>
-						<input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
-					</div> 
-					<button class = 'btn btn-outline-info' type="submit" name="login" value= 'login' class="submit">Login</button>
-				</form>	
-			</div>
-		</div>
-		<div class="overlay-container">
-			<div class="overlay">
-				<div class="overlay-panel overlay-left">
-					<button class="ghost" id="signIn">Sign In</button>
-				</div>
-				<div class="overlay-panel overlay-right">
-					<h1>Hello, Friend!</h1>
-					<p>Enter your personal details and start journey with us</p>
-					<button class="ghost" id="signUp">Sign Up</button>
-				</div>
-			</div>
-		</div>
-
-    </body>
-</html>
+                        <label>Password :</label><br>
+                        <input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
+                    </div> 
+                    <br><button class = 'button-login' type="submit" name="login" value= 'login' class="submit">Login</button>
+                    
+                </form>	
+                <div class="switch">
+                    <br><br><h2> No Account? </h2>
+                    <button class='button-register' >Sign-up</button>
+                </div>
+            </div>
+        </div>
+        <div class="card__face card__face--back">
+            <div class = "form-container">
+                <form method = 'POST' action = 'backend.php' >
+                    <h1 class = "title"> Sign up to !Twitter </h1> <br>
+                    <div class="form-group">
+                        <label>Username : </label><br>
+                        <input class= 'form-control w-25' type="text" name="username"><br>
+                        <label>Password :</label><br>
+                        <input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
+                    </div> 
+                    <br><button class = 'button-login' type="submit" name="register" value= 'login' class="register">Login</button>
+                </form>	
+                <div class="switch">
+                    <br><br><h2> Have an account? </h2>
+                    <button class='button-register1' >Login</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script> 
+        var card = document.querySelector('.card'); 
+        var btn = document.querySelector('.button-register');
+        var btn1 = document.querySelector('.button-register1');
+        btn.addEventListener('click', function () { card.classList.toggle('is-flipped');});
+        btn1.addEventListener('click', function () { card.classList.toggle('is-flipped');}); </script>
+</div>
