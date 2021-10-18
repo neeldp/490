256ecd76425b22085d85e68fb8badce3bfe7c75e
brendan123 <brendan123@users.noreply.github.com>
2021-10-18 10:17:20 -0400

    update backend.php

--- backend.php
+++ backend.php
@@ -1,32 +1,51 @@
 <?php
-
 if($_POST){
     require 'db_key.php';
     $conn = connect_db();
-    if(isset($_POST['login'])){
+    if(isset($_POST['register']) ){
         $username = $_POST['username'];
         $password = $_POST['password'];
-        $passwordHashed = password_hash($password, PASSWORD_BCRYPT);
+        $passwordHashed = password_hash($password, PASSWORD_BCRYPT);    
         $username = mysqli_real_escape_string($conn, $username);
         $passwordHashed = mysqli_real_escape_string($conn, $passwordHashed);
-        $sql = "SELECT * FROM users WHERE username = '$username'";
+        $sql = "Select users.username From users Where username = '$username'";
         $sql = $conn->query($sql);
+        $sql = $sql->fetch_assoc();
         if($sql){
-            $sql = $sql->fetch_assoc();
-            $isAdmin = $sql['isAdmin'];
-            if(password_verify($password, $sql['password'])){
-                session_start();
-                $_SESSION['username'] =$username;
-                $_SESSION['isAdmin'] =$isAdmin;
-                #echo 'Congratulations';
-                header('location: landing.php');
-            }else{
-                header('location: index.php');
-                #header("refresh:2;url=index.php");
-                #echo 'incorrect password';
-                exit();
-            }
-        } 
-    }
+            header('location: /register.php');
+            exit();
+        }else{
+            $sql = "INSERT INTO `users`(`username`, `password`, `isAdmin`) VALUES ('$username','$passwordHashed','1')";
+            $sql = $conn->query($sql);
+        if($sql){
+            echo "Registration succesful. You may <a href= '/'>login</a> now";
+            header('location: index.php');
+        }
+    }   
+}else if(isset($_POST['login'])){
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    $passwordHashed = password_hash($password, PASSWORD_BCRYPT);
+    $username = mysqli_real_escape_string($conn, $username);
+    $passwordHashed = mysqli_real_escape_string($conn, $passwordHashed);
+    $sql = "SELECT * FROM users WHERE username = '$username'";
+    $sql = $conn->query($sql);
+    if($sql){
+        $sql = $sql->fetch_assoc();
+        $isAdmin = $sql['isAdmin'];
+        if(password_verify($password, $sql['password'])){
+            session_start();
+            $_SESSION['username'] =$username;
+            $_SESSION['isAdmin'] =$isAdmin;
+            #echo 'Congratulations';
+            header('location: landing.php');
+        }else{
+            header('location: index.php');
+            #header("refresh:2;url=index.php");
+            #echo 'incorrect password';
+            exit();
+        }
+    } 
+}
 }
 ?>
---
+++ backend0.php
@@ -0,0 +1,32 @@
+<?php
+
+if($_POST){
+    require 'db_key.php';
+    $conn = connect_db();
+    if(isset($_POST['login'])){
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $passwordHashed = password_hash($password, PASSWORD_BCRYPT);
+        $username = mysqli_real_escape_string($conn, $username);
+        $passwordHashed = mysqli_real_escape_string($conn, $passwordHashed);
+        $sql = "SELECT * FROM users WHERE username = '$username'";
+        $sql = $conn->query($sql);
+        if($sql){
+            $sql = $sql->fetch_assoc();
+            $isAdmin = $sql['isAdmin'];
+            if(password_verify($password, $sql['password'])){
+                session_start();
+                $_SESSION['username'] =$username;
+                $_SESSION['isAdmin'] =$isAdmin;
+                #echo 'Congratulations';
+                header('location: landing.php');
+            }else{
+                header('location: index.php');
+                #header("refresh:2;url=index.php");
+                #echo 'incorrect password';
+                exit();
+            }
+        } 
+    }
+}
+?>
