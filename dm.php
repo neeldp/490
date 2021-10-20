c4046698a283e931a8af61640e2aa51bed45ce4f
brendan123 <brendanaszklar@gmail.com>
2021-10-20 11:14:10 -0400

    Update dm.php

--- dm.php
+++ dm.php
@@ -3,22 +3,16 @@ if($_POST){
     require 'db_key.php';
     $conn = connect_db();
     if(isset($_POST['submitMsg'])){
-        $from = $_SESSION['username'];
-        $to = "test";
-        $msg = $_POST['message'];
-        $from = mysqli_real_escape_string($conn, $from);
-        $to = mysqli_real_escape_string($conn, $to);
-        $msg = mysqli_real_escape_string($conn, $msg);
-        $sql = "Select users.username From users Where username = '$from'";
-        $sql = $conn->query($sql);
-        $sql = $sql->fetch_assoc();
-        if($sql){
-            header('location: /direct_Messages.php');
-            exit();
-        }else{
-            $sql = "INSERT INTO `dm_table`(`sender`, `reveiver`, `text_message`) VALUES ('$from','$to','$msg')"; #All new members are default USERS, admins added manually. 
-            $sql = $conn->query($sql);
-        }
+        $sender = $_SESSION['username'];
+        $receiver = "test";
+        $text_message = $_POST['message'];
+        $sender = mysqli_real_escape_string($conn, $sender);
+        $receiver = mysqli_real_escape_string($conn, $receiver);
+        $text_message = mysqli_real_escape_string($conn, $text_message);
+        
+        $sql = "INSERT INTO `dm_table`(`sender`, `reveiver`, `text_message`) VALUES ('$sender','$receiver','$text_message')"; #All new members are default USERS, admins added manually. 
+        sql = $conn->query($sql);
+        
         if($sql){
             echo "Registration succesful. You may <a href= '/'>login</a> now";
             header('location: index.php');
