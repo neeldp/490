5677f982e05f0c649a12e30fd6684ffb3fcbd047
brendan123 <brendanaszklar@gmail.com>
2021-10-20 12:10:47 -0400

    Update dm.php

--- dm.php
+++ dm.php
@@ -4,9 +4,9 @@ if($_POST){
     $conn = connect_db();
     if(isset($_POST['submitMsg'])){
 
-        $sender = mysqli_real_escape_string($_POST['username'], $conn);
-        $receiver = mysqli_real_escape_string($_POST['to'], $conn);
-        $text_message = mysqli_real_escape_string($_POST['message'], $conn);
+        $sender = mysqli_real_escape_string($conn,$_POST['username'] );
+        $receiver = mysqli_real_escape_string($conn,$_POST['to']);
+        $text_message = mysqli_real_escape_string($conn, $_POST['message']);
         $sql = "INSERT INTO 'dm_table' VALUES ('$sender','$text_message','$receiver')";
         $conn -> query($sql);
         echo "BIG POSS";
