e1b725b489f3730d389849ab077713e9a43726cd
brendan123 <brendanaszklar@gmail.com>
2021-10-20 14:54:47 -0400

    Update inbox.php

--- inbox.php
+++ inbox.php
@@ -1,10 +1,14 @@
 <?php require 'header.php';
+        require "db_key.php"
 ?>
 <style>
 th, td {
   padding: 15px;
   text-align: left;
+  border-top: 1px solid #ddd;
   border-bottom: 1px solid #ddd;
+  border-right: 1px solid #ddd;
+  border-left: 1px solid #ddd;
   margin-left: auto;
   margin-right: auto;
 }
@@ -16,6 +20,19 @@ th, td {
                 <th>Message</th>
                 <th>User</th>
             </tr>
+            <?php
+                $conn = connect_db();
+                $sql = "SELECT sender,text_message,receiver FROM dm_table WHERE receiver = '$_SESSION['username']'";
+                $result = $conn->query($sql);
+                if($result -> num_rows > 0){
+                    while($row = $result->fetchassoc()){
+                        echo "<tr><td>" . $row["text_message"] . "</td><td>" . $row["sender"] . "</td></tr>"
+                    }
+                    echo "</table>";
+                } 
+                else { echo "0 results"; }
+                $conn->close();
+            ?>
         </table>
     </div>
 </body>
