d514301912e04fcf13ef3d83e85a204988260f76
neeldp <ndp69@njit.edu>
2021-10-20 00:49:52 -0400

    Update store_Upload.php

--- store_Upload.php
+++ store_Upload.php
@@ -29,8 +29,7 @@ if(isset($_POST["create_Post"])){
     
     $username = $_SESSION['username'];   
     $conn = connect_db();
-    $sql = "Select users.username From users Where username = '$username'";
-    echo $conn;
+     
     $sql = $conn->query($sql);
     $sql = $sql->fetch_assoc();  
     echo $sql;
