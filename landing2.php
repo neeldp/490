df73d197b8e7a05290d42284045c500df3c04df9
brendan123 <brendanaszklar@gmail.com>
2021-10-20 16:57:54 -0400

    Update landing2.php

--- landing2.php
+++ landing2.php
@@ -12,7 +12,7 @@ require 'nav.php';
 	</head>
     
 	<body>	
-		<div class = "posts">
+		<div class>
 			<?php
 				$conn = connect_db();
 				$result = $conn->query("SELECT * FROM posts ORDER BY `time` DESC");
