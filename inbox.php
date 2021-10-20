b846ad54d01310fab9e1446e48d2e11ed27702e8
brendan123 <brendanaszklar@gmail.com>
2021-10-20 14:35:44 -0400

    Update inbox.php

--- inbox.php
+++ inbox.php
@@ -1,11 +1,18 @@
 <?php require 'header.php';
 ?>
+<style>
+th, td {
+  padding: 15px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+  align:center;
+}
+</style>
 <body>
     <table>
         <tr>
             <th>Message</th>
             <th>User</th>
-            <th>country</th>
         </tr>
     </table>
 </body>
