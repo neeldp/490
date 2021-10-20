e3fe4046273ce849f400be358b32a34e1cdd318c
brendan123 <brendanaszklar@gmail.com>
2021-10-20 11:08:12 -0400

    Update header.php

--- header.php
+++ header.php
@@ -4,4 +4,5 @@
     <title> Group 8 Login Screen </title>
     <meta charset = "utf-8" />
     <link rel="stylesheet" href="indexstyles.css" media="screen">
+    <link rel="shortcut icon" href="#">
 </head>
