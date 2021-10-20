e9b3823f2db8b0cfdef862581f678209804194d0
neeldp <ndp69@njit.edu>
2021-10-20 00:20:46 -0400

    Update store_Upload.php

--- store_Upload.php
+++ store_Upload.php
@@ -8,25 +8,17 @@ if(isset($_POST["create_Post"])){
     $status = 'error';
     $post_Text = $_POST["post_Text"];
 
-    if(!empty($_FILES["image"]["name"])) { 
+    if(!empty($_FILES["img"]["name"])) { 
         // Get file info 
-        $fileName = basename($_FILES["image"]["name"]); 
+        $fileName = basename($_FILES["img"]["name"]); 
         $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
          
         // Allow certain file formats 
         $allowTypes = array('jpg','png','jpeg','gif'); 
         if(in_array($fileType, $allowTypes)){ 
-            $image = $_FILES['image']['tmp_name']; 
+            $image = $_FILES['img']['tmp_name']; 
             $imgContent = addslashes(file_get_contents($image)); 
          
-            // Insert image content into database 
-            $insert = $db->query("INSERT into images (image, uploaded) VALUES ('$imgContent', NOW())");
-            if($insert){ 
-                $status = 'success'; 
-                $statusMsg = "File uploaded successfully."; 
-            }else{ 
-                $statusMsg = "File upload failed, please try again."; 
-            }  
         }else{ 
             $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.'; 
         } 
