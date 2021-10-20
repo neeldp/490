d28914e8dd0be1d8d939f29563d175855a8c7311
brendan123 <brendanaszklar@gmail.com>
2021-10-20 16:31:41 -0400

    nav

--- landing2.php
+++ landing2.php
@@ -1,6 +1,7 @@
 <?php session_start();
 require 'header.php';
 require 'db_key.php';
+require 'nav.php'
 ?>
 <!DOCTYPE html>
 <!-- This file is used for the login screen of the CS 490 SEC. 003 Group 8 Project -->
@@ -11,28 +12,22 @@ require 'db_key.php';
 	</head>
     
 	<body>
-       	<h1> <b> USER </h1>
-		<nav>
-        	<ul>
-            	<li><a href="create_Post.php">Create post</a></li>
-            	<li><a href="sendMessage.php">Send a Message.</a></li>
-				<li><a href="inbox.php">Inbox</a></li>
-            	<li><a href="index.php">logout</a></li>
-        	</ul>
-    	</nav>
-		<?php
-			$conn = connect_db();
-			$result = $conn->query("SELECT * FROM posts ORDER BY `time` DESC");
-			if($result->num_rows > 0){
-				while($row = mysqli_fetch_array($result))
-				{
-					echo $row['time']."<br>";
-					echo $row['user']." <br>";
-					echo $row['text']."<br>";
-					echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>'."<br>";
+		
+		<div>
+			<?php
+				$conn = connect_db();
+				$result = $conn->query("SELECT * FROM posts ORDER BY `time` DESC");
+				if($result->num_rows > 0){
+					while($row = mysqli_fetch_array($result))
+					{
+						echo $row['time']."<br>";
+						echo $row['user']." <br>";
+						echo $row['text']."<br>";
+						echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>'."<br>";
+					}
 				}
-			}
-			$conn->close();
-		?>
+				$conn->close();
+			?>
+		</div>
 	</body>
 </html></DOCTYPE>
---
+++ nav.php
@@ -0,0 +1,10 @@
+<header>
+			<nav>
+				<ul>
+					<li><a href="create_Post.php">Create post</a></li>
+					<li><a href="sendMessage.php">Send a Message.</a></li>
+					<li><a href="inbox.php">Inbox</a></li>
+					<li><a href="index.php">logout</a></li>
+				</ul>
+			</nav>
+</header>
