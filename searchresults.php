ba2961848da715e44f6629f15534c3ddb32e9b38
brendan123 <brendanaszklar@gmail.com>
2021-10-20 18:34:38 -0400

    Update searchresults.php

--- searchresults.php
+++ searchresults.php
@@ -18,7 +18,7 @@ require 'nav.php';
             $conn = connect_db();
             if(isset($_POST['search'])){
                 $searchTerm = $_POST['search']; 
-				
+				echo $searchTerm;
             
 				$result = $conn->query("SELECT * FROM posts WHERE `user` LIKE '$searchTerm' OR `text` LIKE '$searchTerm'");
 				if($result->num_rows > 0){
