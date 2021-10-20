244bb493af0e3944c19f9c6941d169c961389a28
brendan123 <brendanaszklar@gmail.com>
2021-10-20 10:54:46 -0400

    Update dm.php

--- dm.php
+++ dm.php
@@ -3,10 +3,10 @@
     $conn = connect_db();
     if(isset($_POST['submitMsg'])){
         $from = $_SESSION['username'];
-        $to = $_POST['to'] ;
+        $to = "test";
         $msg = $_POST['message'];
 
-        $sql = "INSERT INTO 'heroku_d39ddae7fbaabf5.dm_table'('sender','receiver','text_message') VALUES ('$from','$to','$msg')";
+        $sql = "INSERT INTO dm_table ('sender','receiver','text_message') VALUES ('$from','$to','$msg')";
         if($conn->query($sql)==TRUE){
             echo "New record created successfully";
         } else {
