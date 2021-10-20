c4e35cd1d88b375a6a467fd92c24601e394c2233
neeldp <ndp69@njit.edu>
2021-10-19 22:58:09 -0400

    Update create_Post.php

--- create_Post.php
+++ create_Post.php
@@ -11,20 +11,16 @@
     
 	<body>
 		<div class = "post-body"> 
-			<form method = "post" action="backend.php"> <!-- enctype= "multipart/form-data">--> 
-			<textarea class = "status" name = "status" placeholder = "Write your post here!" rows="4" cols="50" maxlength = "300"autofocus></textarea> 
+			<form action="store_Upload.php">
+				<textarea class = "status" name = "status" placeholder = "Write your post here!" rows="4" cols="50" maxlength = "300"autofocus></textarea> 
+				
+				<label for="img">Attach image:</label>
+				<input type="file" id="img" name="img" accept="image/*">
+
+				<button class = 'btn btn-outline-info' type="submit" name="create_Post" value= 'Post' class="submit">Post</button></form>
+			</form>
 		</div>
-		
-		
-		<form action="enctype= multipart/form-data">
-			<label for="img">Attach image:</label>
-			<input type="file" id="img" name="img" accept="image/*">
-			<!-- <input type="submit"> -->
-		</form>
-		
 		<br>
-		
-		<button class = 'btn btn-outline-info' type="submit" name="create_Post" value= 'Post' class="submit">Post</button></form>
-		
+
     	</body>
 </html>
