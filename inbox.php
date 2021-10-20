4b9a7497ce28eca02f0197e9fe80004079f2bd81
brendan123 <brendanaszklar@gmail.com>
2021-10-20 14:30:46 -0400

    update

---
+++ inbox.php
@@ -0,0 +1,11 @@
+<?php require 'header.php';
+?>
+<body>
+    <table>
+        <tr>
+            <th>Message</th>
+            <th>User</th>
+            <th>country</th>
+        </tr>
+    </table>
+</body>
