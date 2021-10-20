2f78ab78e1c9a13e8505d171269913250f3e93ee
brendan123 <brendanaszklar@gmail.com>
2021-10-20 15:48:43 -0400

    Update inbox.php

--- inbox.php
+++ inbox.php
@@ -29,7 +29,7 @@ th, td {
                 $result = $conn->query($sql);
                 if($result -> num_rows > 0){
                     while($row = mysqli_fetch_array($result)){
-                        echo "<tr><td>" . $row['text_message'] . "</td><td>" . $row['sender'] . "</td><td>" . "<form action='backend.php' method = 'POST'><button class = 'btn btn-outline-info' type='delete' name='delete' value= 'delete' class='submit' id='$row['id']'>delete</button></form>". "</td></tr>";
+                        echo "<tr><td>" . $row['text_message'] . "</td><td>" . $row['sender'] . "</td><td>" . "<form action='backend.php' method = 'POST'><button class = 'btn btn-outline-info' type='delete' name='delete' value= 'delete' class='submit' >delete</button></form>". "</td></tr>";
                     }
                     echo "</table>";
                 } 
