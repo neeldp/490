a937f68910737e236061bf4b8abd362827c9b254
brendan123 <brendanaszklar@gmail.com>
2021-10-20 19:10:04 -0400

    test

--- landing.php
+++ landing.php
@@ -9,4 +9,35 @@
     require 'header.php';
     require 'nav.php'
 ?>
-<body> <h1> ADMIN </h1> </body> </html> 
+<body>
+<form method="POST" action='backend.php'>
+			<h3> Search for a post </h3>
+			<div class='form-group'>
+				<label>Search:</label>
+				<input class= 'form-control w-25' type="text" name="admin"><br><br>
+			</div>	
+			<button class = 'btn btn-outline-info' type="submit" name="adminbtn" value= 'message' class="adminbtn">Search</button>
+		</form>
+		<h1 class="tmln"> My Timeline </h1> 
+		<div>
+			<?php
+				$conn = connect_db();
+				$result = $conn->query("SELECT * FROM posts ORDER BY `time` DESC");
+				if($result->num_rows > 0){
+					while($row = mysqli_fetch_array($result))
+					{
+						echo "<div class='posts'>";
+						echo $row['time']."<br>";
+						echo $row['user']." <br>";
+						echo $row['text']."<br>";
+						echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>'."<br>";
+                        echo "<button class = 'btn btn-outline-info' type='submit' name='adminbtn' value= 'message' class='adminbtn'>Admin</button>";
+						echo "</div>";
+						echo "<br><br>";
+					}
+				}
+				$conn->close();
+			?>
+		</div>
+	</body>
+ </html> 
