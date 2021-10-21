d244f3810254055f29f20bb964e4e676b7f38aca
neeldp <ndp69@njit.edu>
2021-10-21 00:53:57 -0400

    Update landing2.php

--- landing2.php
+++ landing2.php
@@ -38,7 +38,7 @@ require 'nav.php';
 						$id = $row['id'];
 						//echo $row['id'];
 						echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>'."<br>";
-						$sql = $conn->query("SELECT * FROM comments Where `postID` = $id ORDER BY `time` DESC");
+						$sql = $conn->query("SELECT * FROM comments Where `postID` = $id ORDER BY `date` DESC");
 						if($sql->num_rows > 0){
 							while($r = mysqli_fetch_array($sql))
 							{
