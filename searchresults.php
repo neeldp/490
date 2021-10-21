81ea9c71e62956f71cf087a9772823c644b02b09
neeldp <ndp69@njit.edu>
2021-10-21 14:49:16 -0400

    Update searchresults.php

--- searchresults.php
+++ searchresults.php
@@ -30,6 +30,30 @@ require 'nav.php';
 						echo $row['user']." <br>";
 						echo $row['text']."<br>";
 						echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>'."<br>";
+						
+						$id = $row['id'];
+						
+						$sql = $conn->query("SELECT * FROM comments Where `post_ID` = '{$id}'");
+					
+						if($sql->num_rows > 0){
+							while($r = mysqli_fetch_array($sql))
+							{
+								echo $r['date']."<br>";
+								echo $r['name'].":";
+								echo $r['text']."<br><br>";
+								
+							}
+						}	
+						echo '<form method="POST" action="backend.php">
+						<div class="form-group">
+							<label>Comment:</label>
+							<input class= "form-control w-25" type="text" name="comment"><br><br>
+							<input type="hidden" name="post_ID" value="'. $row['id'].'" />
+						</div>	
+						<button class = "btn btn-outline-info" type="submit" name="commentbtn" value= "post_Comment">Comment</button>
+					</form>';
+						echo "</div>";
+						echo "<br><br>";
 						echo "</div>";
 						echo "<br><br>";
 					}
