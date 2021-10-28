3e4147e0d63bd2f225c6facfb7f4b436e2aa4dd0
brendan123 <brendanaszklar@gmail.com>
2021-10-28 16:51:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php require 'header.php'; require 'db_key.php';?>
 
-<div class="scene">
-    <div class="card">
+
         <div class="card__face card__face--front">
             <div class = "form-container">
                 <form method = 'POST' action = 'backend.php' >
@@ -16,31 +15,9 @@
                     <br><button class = 'button-login' type="submit" name="login" value= 'login' class="submit">Login</button>
                     
                 </form>	
-                <div class="switch">
-                    <br><br><h2> No Account? </h2>
-                    <button class='button-register' >Sign-up</button>
-                </div>
             </div>
         </div>
-        <div class="card__face card__face--back">
-            <div class = "form-container">
-                <form method = 'POST' action = 'backend.php' >
-                    <h1 class = "title"> Sign up to !Twitter </h1> <br>
-                    <div class="form-group">
-                        <label>Username : </label><br>
-                        <input class= 'form-control w-25' type="text" name="username"><br>
-                        <label>Password :</label><br>
-                        <input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
-                    </div> 
-                    <br><button class = 'button-login' type="submit" name="register" value= 'login' class="register">Sign-up</button>
-                </form>	
-                <div class="switch">
-                    <br><br><h2> Have an account? </h2>
-                    <button class='button-register1' >Login</button>
-                </div>
-            </div>
-        </div>
-    </div>
+        
     
     <script> 
         var card = document.querySelector('.card'); 
