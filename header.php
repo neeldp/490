69d5c6f80b82a5c57c6f8d3e5b7db258b00917d5
brendan123 <brendanaszklar@gmail.com>
2021-10-28 20:26:00 -0400

    update

--- header.php
+++ header.php
@@ -5,4 +5,7 @@
     <meta charset = "utf-8" />
     <link rel="stylesheet" href="indexstyles.css" media="screen">
     <link rel="shortcut icon" href="#">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet"> 
 </head>
