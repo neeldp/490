24f4bb3837c3d3f3cfdf57667033e284205df80a
brendan123 <brendanaszklar@gmail.com>
2021-10-28 21:12:18 -0400

    change

--- landing.php
+++ landing.php
@@ -11,41 +11,61 @@
     require 'db_key.php';
 ?>
 <body>
-<form method="POST" action='searchresults.php'>
+	<!-- Admin Create User -->	
+	<div class = "create_user">
+		<h3> Create A User </h3>
+		<form method = 'POST' action = 'backend.php' >
+			<div class="">
+				<label>Username : </label>
+				<input class= 'form-control w-25' type="text" name="username"><br>
+				<label>Password    :</label>
+				<input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
+			</div> 
+			<br><button class = 'button-login' type="submit" name="register" value= 'login' class="register">Sign-up</button>
+		</form>	
+	</div>
+
+	<!--Search Section --> 
+	<div div class = "search_post">
+		<form method="POST" action='searchresults.php'>
 			<h3> Search for a post </h3>
 			<div class='form-group'>
-            <label>Search:</label>
+				<label>Search:</label>
 				<input class= 'form-control w-25' type="text" name="search"><br><br>
 			</div>	
-			<button class = 'btn btn-outline-info' type="submit" name="searchbtn" value= 'message' class="search">Search</button>
+			<br><button class = 'button-login' type="submit" name="register" value= 'login' class="register">Search</button>
 		</form>
-        <form method="POST" action='backend.php'>
-			<h3> Delete A Post </h3>
-			<div class='form-group'>
-				<label>Delete:</label>
-					<input class= 'form-control w-25' type="text" name="admin"><br><br>
-			</div>	
-			<button class = 'btn btn-outline-info' type="submit" name="adminbtn" value= 'message' class="adminbtn">Admin</button>
-		</form>
-		<h1 class="tmln"> My Timeline </h1> 
-		<div>
-			<?php
-				$conn = connect_db();
-				$result = $conn->query("SELECT * FROM posts ORDER BY `time` DESC");
-				if($result->num_rows > 0){
-					while($row = mysqli_fetch_array($result))
-					{
-						echo "<div class='posts'>";
-						echo $row['time']."<br>";
-						echo $row['user']." <br>";
-						echo $row['text']."<br>";
-						echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>'."<br>";
-						echo "</div>";
-						echo "<br><br>";
-					}
+	</div>
+
+	<!--Delete Post --> 
+    <form method="POST" action='backend.php'>
+		<h3> Delete A Post </h3>
+		<div class='form-group'>
+			<label>Delete:</label>
+				<input class= 'form-control w-25' type="text" name="admin"><br><br>
+		</div>	
+		<button class = 'btn btn-outline-info' type="submit" name="adminbtn" value= 'message' class="adminbtn">Admin</button>
+	</form>
+	
+	<!-- Admin Timeline Code --> 
+	<h1 class="tmln"> My Timeline </h1> 
+	<div>
+		<?php
+			$conn = connect_db();
+			$result = $conn->query("SELECT * FROM posts ORDER BY `time` DESC");
+			if($result->num_rows > 0){
+				while($row = mysqli_fetch_array($result))
+				{
+					echo "<div class='posts'>";
+					echo $row['time']."<br>";
+					echo $row['user']." <br>";
+					echo $row['text']."<br>";
+					echo '<img src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>'."<br>";
+					echo "</div>";
+					echo "<br><br>";
 				}
-				$conn->close();
-			?>
-		</div>
-	</body>
- </html> 
+			}
+			$conn->close();
+		?>
+	</div>
+</body></html> 
--- landing2.php
+++ landing2.php
@@ -8,20 +8,6 @@ require 'nav.php';
 
 <body>
 
-	<!-- Admin Create User -->	
-	<div class = "create_user">
-		<h3> Create A User </h3>
-		<form method = 'POST' action = 'backend.php' >
-			<div class="">
-				<label>Username : </label>
-				<input class= 'form-control w-25' type="text" name="username"><br>
-				<label>Password :</label>
-				<input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
-			</div> 
-			<br><button class = 'button-login' type="submit" name="register" value= 'login' class="register">Sign-up</button>
-		</form>	
-	</div>
-
 	<!--Search Section --> 
 	<div div class = "search_post">
 		<form method="POST" action='searchresults.php'>
