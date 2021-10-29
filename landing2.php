327be4c49d13f9e1650f41007e1ec49733044055
brendan123 <brendanaszklar@gmail.com>
2021-10-28 21:02:44 -0400

    update

--- landing2.php
+++ landing2.php
@@ -13,9 +13,9 @@ require 'nav.php';
 		<h3> Create A User </h3>
 		<form method = 'POST' action = 'backend.php' >
 			<div class="">
-				<label>Username : </label><br>
+				<label>Username : </label>
 				<input class= 'form-control w-25' type="text" name="username"><br>
-				<label>Password :</label><br>
+				<label>Password :</label>
 				<input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
 			</div> 
 			<br><button class = 'button-login' type="submit" name="register" value= 'login' class="register">Sign-up</button>
