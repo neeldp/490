cb7bbe8a8245ce0043ebfa07f1cbf5b4591d11fc
brendan123 <brendanaszklar@gmail.com>
2021-10-30 20:25:25 -0400

    test

--- nav.php
+++ nav.php
@@ -8,5 +8,15 @@
 					<li><a href="inbox.php">Inbox</a></li>
 					<li><a href="index.php">logout</a></li>
 				</ul>
+				<!--Search Section --> 
+				<div div class = "search_post">
+					<form method="POST" action='searchresults.php'>
+						<div class='form-group'>
+							<label>Search:</label>
+							<input class= 'form-control w-25' type="text" name="search">
+						</div>	
+						<sp><button class = 'button-login' type="submit" name="register" value= 'login' class="register">Search</button>
+					</form>
+				</div>
 			</nav>
 </header>
