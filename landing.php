b4456caef99cbe2d02b4395e59384998be37b2cd
brendan123 <brendanaszklar@gmail.com>
2021-11-02 22:03:15 -0400

    Update landing.php

--- landing.php
+++ landing.php
@@ -36,7 +36,7 @@
     echo "<form method='POST' action='backend.php'>";
 		echo "<h3>". "Delete A Post". "</h3>";
 		echo "<div class='form-group'>";
-			echo "<label>"."Delete:""</label>";
+			echo "<label>"."Delete:"."</label>";
 				echo "<input class= 'form-control w-25' type='text' name='admin'>"."<br>"."<br>";
 		echo "</div>";	
 		echo "<button class = 'btn btn-outline-info' type='submit' name='adminbtn' value= 'message' class='adminbtn'>"."Admin"."</button>";
