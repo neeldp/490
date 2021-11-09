68dba23fbc2187109db19c428734c6bc31c4ae29
brendan123 <brendanaszklar@gmail.com>
2021-11-09 00:29:30 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -23,6 +23,14 @@
 				</form>
 			</div>
 			</li>	
+			<li>
+				<p>
+				<a href="http://jigsaw.w3.org/css-validator/check/referer">
+					<img style="border:0;width:88px;height:31px"
+						src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+						alt="valid css" >
+					</a>
+				</p>
 		</ul>
 	</nav>
 </header>
