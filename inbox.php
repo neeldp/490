747978c46ea16dfcbe0eead6e1da3b31f36db3e4
brendan123 <brendanaszklar@gmail.com>
2021-11-11 11:49:15 -0500

    Update inbox.php

--- inbox.php
+++ inbox.php
@@ -2,32 +2,22 @@
 require 'header.php';
 require "db_key.php";
 require 'nav.php';
-require 'backend.php';
 ?>
 
 <body>
     <div class="inbox_style">
-        <table>
-            <tr>
-                <th>Message</th>
-                <th>User</th>
-            </tr>
-            <?php
-                $conn = connect_db();
-                $usr = $_SESSION['username'];
-                $sql = "SELECT sender,text_message,receiver FROM dm_table WHERE `receiver` = '$usr'";
-                $result = $conn->query($sql);
-                if($result -> num_rows > 0){
-                    while($row = mysqli_fetch_array($result)){
-                        $id = $row['id'];
-                        echo "<tr><td>" . $row['text_message'] . "</td><td>" . $row['sender'] . "</td></tr>";
-                    }
-                    echo "</table>";
-                } 
-                else { echo "0 results"; }
-                $conn->close();  
-            ?>
-        </table>
+        <?php
+           $conn = connect_db();
+           $usr = $_SESSION['username']; 
+           $sql = "SELECT sender,text_message,receiver,id FROM dm_table WHERE `receiver` = '$usr'";
+           $result = $conn->query($sql);
+           if($result -> num_rows > 0){
+            while($row = mysqli_fetch_array($result)){
+                echo "<p>" . $row['text_message'] . "</br>" . $row['sender'] . "<br><br>";
+            }
+        }
+        $conn->close();
+        ?>
         <div class ="form-container message">
 			<form method='POST' action ='backend.php'>
 				<div class='form-group'>
