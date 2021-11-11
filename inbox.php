621276a37a039e3cb88b22e376f3f5ef8ccfe49e
brendan123 <brendanaszklar@gmail.com>
2021-11-11 00:57:48 -0500

    Update inbox.php

--- inbox.php
+++ inbox.php
@@ -29,12 +29,11 @@ require 'nav.php';
         </table>
         <div class ="form-container message">
 			<form method='POST' action ='backend.php'>
-				<h1> Send a Message </h1>
 				<div class='form-group'>
 					<label>To:</label>
-					<input class= 'form-control w-25' type="text" name="to"><br><br>
+					<input class= 'form-control w-25' type="text" name="to">
 					<label>Message:</label>
-					<input class= 'form-control w-25' type="text" name="message"><br><br>
+					<input class= 'form-control w-25' type="text" name="message">
                 </div>
 				<button class = 'btn btn-outline-info' type="submit" name="submitMsg" value= 'message' class="submitMsg">Message</button>
 			</form>
