532646ae9e801661f7f78e0c3de15b17acfce7e5
brendan123 <brendanaszklar@gmail.com>
2021-11-12 09:18:29 -0500

    Update conversation.php

--- conversation.php
+++ conversation.php
@@ -3,5 +3,36 @@ require 'header.php';
 require "db_key.php";
 require 'nav.php';
 $NAME = $_GET['NAME'];
-echo 'Hello ' . htmlspecialchars($_GET["name"]) . '!';
+echo 'Conversation with ' . htmlspecialchars($_GET["name"]) . '!';
 ?>
+
+<body>
+    <div class="inbox_style">
+        <?php
+           $conn = connect_db();
+           $usr = $_SESSION['username'];
+
+           $sql = "SELECT sender,text_message,receiver,id FROM dm_table WHERE `receiver` = '$usr' AND `sender` = '$NAME'";
+           $result = $conn->query($sql);
+           if($result -> num_rows > 0){
+               while($row = mysqli_fetch_array($result)){
+                   echo "<p> " . $row['text_message'] . "</br>" . $row['sender'] . "<br><br>" . "</p>";
+                }
+            }
+            
+
+        $conn->close();
+        ?>
+        <div class ="form-container message">
+            <form method='POST' action ='backend.php'>
+                <div class='form-group'>
+                    <label>To:</label>
+                    <input class= 'form-control w-25' type="text" name="to">
+                    <label>Message:</label>
+                    <input class= 'form-control w-25' type="text" name="message">
+                </div>
+                <button class = 'btn btn-outline-info' type="submit" name="submitMsg" value= 'message' class="submitMsg">Message</button>
+            </form>
+        </div>    
+    </div>
+</body>
