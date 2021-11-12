c77adfabfe0c68b2012296b119c07990cc53f24d
neeldp <ndp69@njit.edu>
2021-11-12 10:32:48 -0500

    Update inbox.php

--- inbox.php
+++ inbox.php
@@ -12,15 +12,15 @@ require 'nav.php';
         $conn = connect_db();
         $usr = $_SESSION['username'];
         //$sql = "SELECT sender FROM dm_table WHERE receiver = '{$usr}'";
-        $sql = "SELECT receiver FROM dm_table WHERE sender = '{$usr}'";
+        $sql = "SELECT receiver FROM dm_table WHERE sender = '{$usr}' UNION SELECT sender FROM dm_table WHERE receiver = '{$usr}'";
         $result = $conn->query($sql);
         if($result -> num_rows > 0){
             while($row = mysqli_fetch_array($result)){
-                if ($row['sender'] == $usr ){
+                //if ($row['sender'] == $usr ){
                 echo "<tr><td>".  "<a href='conversation.php?name=".$row['receiver'] . "'>" . $row['receiver']  . "</a></td></tr>";
-                }else{
-                    echo "<tr><td>".  "<a href='conversation.php?name=".$row['sender'] . "'>" . $row['sender']  . "</a></td></tr>";
-                }
+                //}else{
+                //    echo "<tr><td>".  "<a href='conversation.php?name=".$row['sender'] . "'>" . $row['sender']  . "</a></td></tr>";
+                //}
              }
         }
          $conn->close();
