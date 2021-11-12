6b10fa125beea56e730222420c335bad9f687257
brendan123 <brendanaszklar@gmail.com>
2021-11-11 19:21:38 -0500

    t

--- inbox.php
+++ inbox.php
@@ -15,7 +15,7 @@ require 'nav.php';
         $result = $conn->query($sql);
         if($result -> num_rows > 0){
             while($row = mysqli_fetch_array($result)){
-                echo "<td> <a> " .$row['sender']  . "</a></td>";
+                echo "<td> <a> " . $row['sender']  . "</a></td>";
              }
          }
         ?>
--- sendMessage.php
+++ sendMessage.php
@@ -5,23 +5,17 @@ require 'nav.php';
 <!-- This file is used for creating a post for the CS 490 SEC. 003 Group 8 Simple Social Media Project -->
 
 	<body>
-		<?php
-		require header.php;
-		if($_POST){
-			require 'db_key.php';
-			$conn = connect_db();
-			if(isset($_POST['submitMsg'])){
-				$sender = mysqli_real_escape_string($conn,$_POST['username'] );
-				$receiver = mysqli_real_escape_string($conn,$_POST['to']);
-				$text_message = mysqli_real_escape_string($conn, $_POST['message']);
-				$sql = "INSERT INTO 'dm_table'('sender','text_message','receiver') VALUES ('$sender','$text_message','$receiver')";
-				$result = $conn->query($sql);
-				if(!$result){
-					die("Invalid ". mysql_error());
-				}
-				}
-			}
-		?>	
+	<div class ="form-container message">
+            <form method='POST' action ='backend.php'>
+                <div class='form-group'>
+                    <label>To:</label>
+                    <input class= 'form-control w-25' type="text" name="to">
+                    <label>Message:</label>
+                    <input class= 'form-control w-25' type="text" name="message">
+                </div>
+                <button class = 'btn btn-outline-info' type="submit" name="submitMsg" value= 'message' class="submitMsg">Message</button>
+            </form>
+        </div>    
 		
     </body>
 </html>
