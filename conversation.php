2ac4be1fa4f574a53ebba026da1561d7b615aad6
neeldp <ndp69@njit.edu>
2021-11-12 11:13:52 -0500

    Update conversation.php

--- conversation.php
+++ conversation.php
@@ -12,21 +12,21 @@ $NAME = $_GET["name"];
            $conn = connect_db();
            $usr = $_SESSION['username'];
 
-           $sql = "SELECT sender,text_message,receiver,id FROM dm_table WHERE `receiver` = '{$usr}' AND `sender` = '{$NAME}'";
+           $sql = "SELECT sender,text_message,receiver,id FROM dm_table WHERE (`receiver` = '{$usr}' OR `receiver` = '{$NAME}')  AND (`sender` = '{$NAME}' OR `sender` = '{$usr}')";
            $result = $conn->query($sql);
            if($result -> num_rows > 0){
                while($row = mysqli_fetch_array($result)){
                    echo "<p> " . $row['text_message'] . "</br>" . $row['sender'] . "<br><br>" . "</p>";
                 }
             }
-            echo "<p> Should be current users sent messages </p>";
-            $sql = "SELECT sender,text_message,receiver,id FROM dm_table WHERE `receiver` = '{$NAME}' AND `sender` = '{$usr}'";
-            $result = $conn->query($sql);
-            if($result -> num_rows > 0){
-                while($row = mysqli_fetch_array($result)){
-                    echo "<p> " . $row['text_message'] . "</br>" . $row['sender'] . "<br><br>" . "</p>";
-                 }
-             }
+            //echo "<p> Should be current users sent messages </p>";
+            //$sql = "SELECT sender,text_message,receiver,id FROM dm_table WHERE `receiver` = '{$NAME}' AND `sender` = '{$usr}'";
+            //$result = $conn->query($sql);
+            //if($result -> num_rows > 0){
+            //    while($row = mysqli_fetch_array($result)){
+            //        echo "<p> " . $row['text_message'] . "</br>" . $row['sender'] . "<br><br>" . "</p>";
+            //     }
+            // }
         $conn->close();
         ?>
         <div class ="form-container message">
