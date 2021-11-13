253529fe51b504946fc8f56423a25ce73a549eb7
brendan123 <brendan123@users.noreply.github.com>
2021-11-13 15:30:03 -0500

    Update header.php

--- header.php
+++ header.php
@@ -8,6 +8,7 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet"> 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 </head>
 <?php
 ini_set('session.cookie_httponly', 1);
