5cc63fafe3597103c24a194482374df387ae7b47
neeldp <ndp69@njit.edu>
2021-11-14 01:57:52 -0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -6,7 +6,22 @@ require 'db_key.php';
 
 
 <body>
+    <div class = "profile">
     <?php
     $user = $_SESSION['username'];
-    echo $user;    
+    echo $user; 
+    
+    $conn = connect_db();
+	$result = $conn->query("SELECT * FROM posts WHERE username = '$user' ORDER BY `time` DESC");
+	if($result->num_rows > 0){
+	    while($row = mysqli_fetch_array($result))
+		{
+		    echo "<div class='posts'>";
+			echo '<img class="post_image" src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
+			echo "<p class='ptime'>". $row['time']."</p>";
+        }
+    }
     ?>
+    <div>
+<body>
+        
