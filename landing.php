c8ea930dafc4c3c2fbde1ce78b7d2738e30d9b74
neeldp <ndp69@njit.edu>
2021-11-15 14:18:42 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -27,7 +27,7 @@
 		$user = $_SESSION['username'];
 		$conn = connect_db();
 		$id = $conn->query("SELECT id FROM users WHERE `username` != '{$user}'");
-		echo $id;
+		echo "$id";
 		$result = $conn->query("SELECT * FROM users WHERE `username` != '{$user}'");
 		if($result->num_rows > 0){
 			while($row = mysqli_fetch_array($result))
