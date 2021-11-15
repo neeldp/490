3643c51953514e06b0d0bff6d4d8e471c8d454ed
neeldp <ndp69@njit.edu>
2021-11-15 14:33:08 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -26,8 +26,10 @@
 
 		$user = $_SESSION['username'];
 		$conn = connect_db();
-		$id = $conn->query("SELECT id FROM users WHERE `username` = '{$user}'");
-		echo "$id";
+		$result = $conn->query("SELECT id FROM users WHERE `username` = '{$user}'");
+		$row = mysqli_fetch_array($result);
+		$follower_id = $row['id'];
+
 		$result = $conn->query("SELECT * FROM users WHERE `username` != '{$user}'");
 		if($result->num_rows > 0){
 			while($row = mysqli_fetch_array($result))
