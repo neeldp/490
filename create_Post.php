be8607289ce0186fb6c2d71cbd527cfe8c0405fe
brendan123 <brendanaszklar@gmail.com>
2021-11-15 16:32:27 -0500

    Update create_Post.php

--- create_Post.php
+++ create_Post.php
@@ -26,9 +26,9 @@ require 'db_key.php';
 		echo "<p> break </p>";
 
 		$trk = $api->getTrack('4uLU6hMCjMI75M1A2tKUQC');
-		$key = array_search("https://open.spotify.com/track/4uLU6hMCjMI75M1A2tKUQC",$trk);
-		echo "<br>";
-		echo '<b>' . $key . '</b>';
+		$json = json_decode($trk,true,4)
+		var_dump(json_decode($json, true, 4));
+		echo 'Last error: ', json_last_error_msg(), PHP_EOL, PHP_EOL;
 	?> 
 	<div class = "post-body"> 
 		<form action="backend.php" method="POST" enctype="multipart/form-data">
