26181e9bacaeb76a57c64c369acb67ac59059c2e
brendan123 <brendanaszklar@gmail.com>
2021-11-15 17:30:44 -0500

    Update create_Post.php

--- create_Post.php
+++ create_Post.php
@@ -23,9 +23,11 @@ require 'db_key.php';
 
 		 $res = $api->search('hotel', ['track']);
 
-		echo "<p>" . $res->name ." </p>";
+		echo "<p>" . $res ." </p>";
 
+		$trk = $api->getTrack('4uLU6hMCjMI75M1A2tKUQC');
 		
+		echo '<b>'	. $trk->href . '</b>';
 	?> 
 	<div class = "post-body"> 
 		<form action="backend.php" method="POST" enctype="multipart/form-data">
@@ -35,28 +37,5 @@ require 'db_key.php';
 			<button class = 'btn btn-outline-info' type="submit" name="create_Post" value= 'Post' class="submit">Post</button></form>
 		</form>
 	</div>
-	<!-- <script>
-		var client_id = 'b7d9baca79b6424597551d19d5fd02cf';
-		var client_secret = '6b988bb0c7ae4cf58013ff29e6ce5a26';
-		var buffer = new Buffer(); 
-		var authOptions = {
-			url: 'https://accounts.spotify.com/api/token',
-			headers: {
-			'Authorization': 'Basic ' + (buffer.from(client_id + ':' + client_secret).toString('base64'))
-			},
-			form: {
-			grant_type: 'client_credentials'
-			},
-			json: true
-		};
 
-		request.post(authOptions, function(error, response, body) {
-			if (!error && response.statusCode === 200) {
-			var token = body.access_token;
-			}
-		});
-
-		process.env.SPOTIFY_TOKEN = token;
-		console.log(token);
-	</script>  -->
 </body></html>
