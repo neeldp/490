d113c1b77378587815da157efa9c7f774a2746e7
brendan123 <brendanaszklar@gmail.com>
2021-11-15 13:09:18 -0500

    t

--- create_Post.php
+++ create_Post.php
@@ -4,7 +4,23 @@ require 'db_key.php';
 ?>   
 <body>
 	<?php 
+		require 'vendor/autoload.php';
+
+		$session = new SpotifyWebAPI\Session(
+			'b7d9baca79b6424597551d19d5fd02cf',
+			'6b988bb0c7ae4cf58013ff29e6ce5a26',
+			'https://login490.herokuapp.com/redir.php'
+		);
+		
+		$api = new SpotifyWebAPI\SpotifyWebAPI();
+		
+		$session->requestCredentialsToken();
+		$accessToken = $session->getAccessToken();
+		
+		// Set the code on the API wrapper
+		$api->setAccessToken($accessToken);
 		echo "<p>" . $_SESSION[accesstoken] . "</p>"
+
 	?> 
 	<div class = "post-body"> 
 		<form action="backend.php" method="POST" enctype="multipart/form-data">
--- redir.php
+++ redir.php
@@ -4,7 +4,7 @@ $session = new SpotifyWebAPI\Session(
     'b7d9baca79b6424597551d19d5fd02cf',
     '6b988bb0c7ae4cf58013ff29e6ce5a26',
     'https://login490.herokuapp.com/redir.php'
-)
+);
 
 $api = new SpotifyWebAPI\SpotifyWebAPI();
 
