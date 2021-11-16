06f838aa0dcc8edf38253da1b3475e7ff86da795
jakeN <76239912+jtn24-NJIT@users.noreply.github.com>
2021-11-16 03:03:02 -0500

    Added inbox container div

--- conversation.php
+++ conversation.php
@@ -16,38 +16,39 @@ $_SESSION['get'] = $NAME;
     }, 1000);
     });
     </script>
-    <div class="inbox_style" id="fresh">
-        <?php
-           $conn = connect_db();
-           $usr = $_SESSION['username'];
+    <div class="inbox_container_style">
+        <div class="inbox_style" id="fresh">
+            <?php
+               $conn = connect_db();
+               $usr = $_SESSION['username'];
 
-           $sql = "SELECT sender,text_message,receiver,id FROM dm_table WHERE (`receiver` = '{$usr}' OR `receiver` = '{$NAME}')  AND (`sender` = '{$NAME}' OR `sender` = '{$usr}')";
-           $result = $conn->query($sql);
-           if($result -> num_rows > 0){
-               while($row = mysqli_fetch_array($result)){
-                   echo "<p> " . $row['text_message'] . "</br>" . $row['sender'] . "<br><br>" . "</p>";
+               $sql = "SELECT sender,text_message,receiver,id FROM dm_table WHERE (`receiver` = '{$usr}' OR `receiver` = '{$NAME}')  AND (`sender` = '{$NAME}' OR `sender` = '{$usr}')";
+               $result = $conn->query($sql);
+               if($result -> num_rows > 0){
+                   while($row = mysqli_fetch_array($result)){
+                       echo "<p> " . $row['text_message'] . "</br>" . $row['sender'] . "<br><br>" . "</p>";
+                    }
                 }
-            }
-            //echo "<p> Should be current users sent messages </p>";
-            //$sql = "SELECT sender,text_message,receiver,id FROM dm_table WHERE `receiver` = '{$NAME}' AND `sender` = '{$usr}'";
-            //$result = $conn->query($sql);
-            //if($result -> num_rows > 0){
-            //    while($row = mysqli_fetch_array($result)){
-            //        echo "<p> " . $row['text_message'] . "</br>" . $row['sender'] . "<br><br>" . "</p>";
-            //     }
-            // }
-        $conn->close();
-        ?>
-        </div>
-        <div class ="form-container message">
-            <form method='POST' action ='backend.php'>
-                <div class='form-group'>
-                    <label>Message:</label>
-                    <input class= 'form-control w-25' type="text" name="message">
-                </div>
-                <button class = 'btn btn-outline-info' type="submit" name="replyMsg" value= 'message' class="replyMsg">Message</button>
-            </form>
-        </div> 
-        
-
+                //echo "<p> Should be current users sent messages </p>";
+                //$sql = "SELECT sender,text_message,receiver,id FROM dm_table WHERE `receiver` = '{$NAME}' AND `sender` = '{$usr}'";
+                //$result = $conn->query($sql);
+                //if($result -> num_rows > 0){
+                //    while($row = mysqli_fetch_array($result)){
+                //        echo "<p> " . $row['text_message'] . "</br>" . $row['sender'] . "<br><br>" . "</p>";
+                //     }
+                // }
+            $conn->close();
+            ?>
+            </div>
+    </div>
+     
+    <div class ="form-container message">
+        <form method='POST' action ='backend.php'>
+            <div class='form-group'>
+                <label>Message:</label>
+                <input class= 'form-control w-25' type="text" name="message">
+            </div>
+            <button class = 'btn btn-outline-info' type="submit" name="replyMsg" value= 'message' class="replyMsg">Message</button>        
+        </form>
+    </div> 
 </body>
