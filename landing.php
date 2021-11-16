4dfa2e539480904e948ce789ee354d59ea8f7991
neeldp <ndp69@njit.edu>
2021-11-16 00:27:06 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -56,8 +56,19 @@
             echo "<p>". $value."</p>";
         ?>
         <?php
+            $user = $_SESSION['username'];
             $conn = connect_db();
-            $result =
+            $result = $conn->query("SELECT id FROM users WHERE `username` = '{$user}'");
+            $row = mysqli_fetch_array($result);
+		    $user_id = $row['id'];
+            $sql = $conn->query("SELECT `user` FROM followers_table WHERE follower_id = '{$user_id}'");
+            if($sql->num_rows > 0){
+                while($row = mysqli_fetch_array($sql))
+                {
+                    echo $row['user'];
+
+                }
+            }
             $result = $conn->query("SELECT * FROM posts ORDER BY `time` DESC");
             if($result->num_rows > 0){
                 while($row = mysqli_fetch_array($result))
