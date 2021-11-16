a4b397c026e9de97dba62443927c7de2daa075f9
neeldp <ndp69@njit.edu>
2021-11-16 15:37:33 -0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -29,7 +29,7 @@ require 'db_key.php';
 			$row = mysqli_fetch_array($sql_query);
 			//echo "$row[0]";
 			//echo '<a href="following.php">following</a>';
-			echo "$row[0]";
+			echo "$row[0] ";
 			echo '<label onclick="myFunction()">Following</label';
 			
 
