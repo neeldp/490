61cd83577dde9350cb3a85e30570c68fb7562131
neeldp <ndp69@njit.edu>
2021-11-16 17:26:58 -0500

    Update followers.php

--- followers.php
+++ followers.php
@@ -10,21 +10,31 @@ require 'db_key.php';
     $conn = connect_db();
 
     $sql = $conn->query("SELECT distinct `id` FROM followers_table WHERE `user` = '{$user}'");
-    //$counter = 0;
-    //$arr = array();
+    $counter = 0;
+    $arr = array();
     if($sql->num_rows > 0){
         while($row = mysqli_fetch_array($sql))
         {
             $id = $row['id'];
             //echo $id;
-            $result = $conn->query("SELECT `username` FROM `users` WHERE `id` = '{$id}'");
-            $record = mysqli_fetch_array($result);
+            //$result = $conn->query("SELECT `username` FROM `users` WHERE `id` = '{$id}'");
+            //$record = mysqli_fetch_array($result);
             //$follower = $record[0];
+            $arr[$counter] = $id;
+            $counter++;
 
-            echo "$record";
-            echo '<br>';
-        
+            //echo "$record";
+            //echo '<br>'; 
         }
+        $list = implode("' ,'", $arr);
+        $sql_query = $conn->query("SELECT username FROM `users` Where `id` IN ('{$list}')");
+        while($row = mysqli_fetch_array($sql_query))
+        {
+            echo $row['username'];
+            echo '<br>';
+
+        }
+
     }
 
 ?>
