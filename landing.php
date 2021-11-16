0eb0d1e1af451b535963b7da3173080fa24e525e
neeldp <ndp69@njit.edu>
2021-11-16 01:43:46 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -73,9 +73,9 @@
 
                 }
             }
-            $list = implode(',', $arr);
+            $list = implode("' ,'", $arr);
             //echo $list;
-            $sql_query = $conn->query("SELECT * FROM `posts` Where `user` IN ('jake', 'admin', 'adminn', 'adminnn') ORDER BY `time` DESC");
+            $sql_query = $conn->query("SELECT * FROM `posts` Where `user` IN ({$list}) ORDER BY `time` DESC");
             if($sql_query->num_rows > 0){
                 while($row = mysqli_fetch_array($sql_query))
                 {
