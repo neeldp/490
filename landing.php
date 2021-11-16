569e29d6159a1b0e764007906ba0681718144ede
neeldp <ndp69@njit.edu>
2021-11-15 22:53:35 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -4,103 +4,99 @@
     require 'db_key.php';
 ?>
 <body>
-	<?php
-	if($_SESSION['isAdmin'] == 1){
-		echo "<form method='POST' action='backend.php'>";
-		echo "<h3>". "Delete A Post". "</h3>";
-		echo "<div class='form-group'>";
-		echo "<label>"."Delete:"."</label>";
-		echo "<input class= 'form-control w-25' type='text' name='admin'>"."<br>"."<br>";
-		echo "</div>";	
-		echo "<button class = 'btn btn-outline-info' type='submit' name='adminbtn' value= 'message' class='adminbtn'>"."Admin"."</button>";
-		echo "</form>";
-	}else{
-		echo "<br>";
-	}
+    <?php
+    if($_SESSION['isAdmin'] == 1){
+        echo "<form method='POST' action='backend.php'>";
+        echo "<h3>". "Delete A Post". "</h3>";
+        echo "<div class='form-group'>";
+        echo "<label>"."Delete:"."</label>";
+        echo "<input class= 'form-control w-25' type='text' name='admin'>"."<br>"."<br>";
+        echo "</div>";    
+        echo "<button class = 'btn btn-outline-info' type='submit' name='adminbtn' value= 'message' class='adminbtn'>"."Admin"."</button>";
+        echo "</form>";
+    }else{
+        echo "<br>";
+    }
 
-	?>
+    ?>
 
-	<div class = "follow list">
+    <div class = "follow list">
 
-		<?php
+        <?php
 
-		$user = $_SESSION['username'];
-		$conn = connect_db();
-		$result = $conn->query("SELECT * FROM users WHERE `username` != '{$user}'");
-		if($result->num_rows > 0){
-			while($row = mysqli_fetch_array($result))
-			{
-				echo $row['username'];
-				//echo '<button type="button>Follow"</button>';
+        $user = $_SESSION['username'];
+        $conn = connect_db();
+        $result = $conn->query("SELECT * FROM users WHERE `username` != '{$user}'");
+        if($result->num_rows > 0){
+            while($row = mysqli_fetch_array($result))
+            {
+                echo $row['username'];
+                //echo '<button type="button>Follow"</button>';
 
-				//echo '<button onclick="myFunction()>"Follow"</button>';
-				
-				echo '<form method="POST" action="backend.php">
-				<input type="hidden" name="user" value="'. $row['username'].'" />
-				<button class = "btn btn-outline-info" type="submit" name="followbtn" value= "follower">Follow</button>
-				</form>';
+                //echo '<button onclick="myFunction()>"Follow"</button>';
+                
+                echo '<form method="POST" action="backend.php">
+                <input type="hidden" name="user" value="'. $row['username'].'" />
+                <button class = "btn btn-outline-info" type="submit" name="followbtn" value= "follower">Follow</button>
+                </form>';
 
-				echo '<br>';
-				
-			}
-		}
+                echo '<br>';
+                
+            }
+        }
 
-		?>
-	</div>	
+        ?>
+    </div>    
 
-	<!-- Timeline Code --> 
-	<div class = "timeline">
-		<h1> My Timeline </h1> 
-		<?php
-			$value = getenv("SPOTIFY_TOKEN");
-			echo "<p>". $value."</p>";
-		?>
-		<?php
-			$conn = connect_db();
-			$result =
-			$result = $conn->query("SELECT * FROM posts ORDER BY `time` DESC");
-			if($result->num_rows > 0){
-				while($row = mysqli_fetch_array($result))
-				{
-					echo "<div class='posts'>";
-					echo "<p class='puser'>". $row['user']."</p>";
-					echo '<img class="post_image" src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
-					echo "<p class='ptext'>". $row['text']."</p>";
-					echo "<p class='ptime'>". $row['time']."</p>";
-					echo "<iframe src='https://open.spotify.com/embed/track/". $row['spotID'] . "'" . 'width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>';
-						
+    <!-- Timeline Code --> 
+    <div class = "timeline">
+        <h1> My Timeline </h1> 
+        <?php
+            $value = getenv("SPOTIFY_TOKEN");
+            echo "<p>". $value."</p>";
+        ?>
+        <?php
+            $conn = connect_db();
+            $result =
+            $result = $conn->query("SELECT * FROM posts ORDER BY `time` DESC");
+            if($result->num_rows > 0){
+                while($row = mysqli_fetch_array($result))
+                {
+                    echo "<div class='posts'>";
+                    echo "<p class='puser'>". $row['user']."</p>";
+                    echo '<img class="post_image" src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
+                    echo "<p class='ptext'>". $row['text']."</p>";
+                    echo "<p class='ptime'>". $row['time']."</p>";
+                    echo "<iframe src='https://open.spotify.com/embed/track/". $row['spotID'] . "'" . 'width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>';
+                        
 
-					$id = $row['id'];
-					$sql = $conn->query("SELECT * FROM comments Where post_ID = '{$id}'");
+                    $id = $row['id'];
+                    $sql = $conn->query("SELECT * FROM comments Where post_ID = '{$id}'");
 
-					if($sql->num_rows > 0){
-						while($r = mysqli_fetch_array($sql))
-						{
-							echo $r['date']."<br>";
-							echo $r['name'].":";
-							echo $r['text']."<br><br>";
+                    if($sql->num_rows > 0){
+                        while($r = mysqli_fetch_array($sql))
+                        {
+                            echo $r['date']."<br>";
+                            echo $r['name'].":";
+                            echo $r['text']."<br><br>";
 
-						}
-					}
-					echo '<form method="POST" action="backend.php">';
-					echo '<div class ="form-group">';
-					echo '<label>Comment:</label>';
-					echo '<input class= "form-control w-25" type="text" name="comment">';
-					echo '<input type="hidden" name="post_ID" value="'. $row['id'].'" />';
-					echo '</div>';
-					echo '<button class = "btn btn-outline-info" type="submit" name="commentbtn" value= "post_Comment">Comment</button>';
-					echo '</form>';
+                        }
+                    }
+                    echo '<form method="POST" action="backend.php">';
+                    echo '<div class ="form-group">';
+                    echo '<label>Comment:</label>';
+                    echo '<input class= "form-control w-25" type="text" name="comment">';
+                    echo '<input type="hidden" name="post_ID" value="'. $row['id'].'" />';
+                    echo '</div>';
+                    echo '<button class = "btn btn-outline-info" type="submit" name="commentbtn" value= "post_Comment">Comment</button>';
+                    echo '</form>';
 
-					echo "</div>";
-					echo "<br><br>";
-				}
-			}
-			
-			$conn->close();
-		?>
-	</div>
-	
-	<iframe src="https://open.spotify.com/embed/playlist/0WZVzE2o5qYnCiEfIDA4rD?utm_source=generator" width="100%" height="80" frameBorder="0"
-	allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture">
-	</iframe>
+                    echo "</div>";
+                    echo "<br><br>";
+                }
+            }
+            
+            $conn->close();
+        ?>
+    </div>
 </body></html> 
