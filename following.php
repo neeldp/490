14f2bb8fcd0ca5dd6ef5ecd25781b1e4a4c629f9
neeldp <ndp69@njit.edu>
2021-11-16 15:45:07 -0500

    Update following.php

--- following.php
+++ following.php
@@ -6,6 +6,20 @@ require 'db_key.php';
 
 <?php
 
-    echo "hi";
+    $user = $_SESSION['username'];
+    $conn = connect_db();		
+    $result = $conn->query("SELECT id FROM users WHERE `username` = '{$user}'");
+    $row = mysqli_fetch_array($result);
+	$user_id = $row['id'];
+
+    $sql = $conn->query("SELECT distinct `user` FROM followers_table WHERE follower_id = '{$user_id}'");
+    if($sql->num_rows > 0){
+        while($row = mysqli_fetch_array($sql))
+        {
+            echo $row['user'];
+            echo '<br>';
+        
+        }
+    }
 
 ?>
