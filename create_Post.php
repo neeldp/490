aeccc2052a5e76fc465cce3708e164792fb66454
brendan123 <brendan123@users.noreply.github.com>
2021-11-15 20:29:12 -0500

    Update create_Post.php

--- create_Post.php
+++ create_Post.php
@@ -6,6 +6,14 @@ $songID = $_GET['id'];
 $_SESSION['song'] = $NAME;
 ?>   
 <body>
+<div class = "post-body"> 
+		<form action="backend.php" method="POST" enctype="multipart/form-data">
+			<textarea class = "status" name = "post_Text" placeholder = "Write your post here!" rows="4" cols="50" maxlength = "300"autofocus></textarea> 
+			<label for="img">Attach image:</label>
+			<input type="file" id="img" name="img" accept="image/*">
+			<button class = 'btn btn-outline-info' type="submit" name="create_Post" value= 'Post' class="submit">Post</button></form>
+		</form>
+	
 <form method="POST" action='backend.php'>
 	<div class='form-group'>
 		<label>Search for a Song on Spotify:</label>
@@ -13,6 +21,7 @@ $_SESSION['song'] = $NAME;
 	</div>	
 	<sp><button type="submit" name="searchbtnsong" value= 'searchbtn' class="searchbtnsong">Search</button>
 </form>
+</div>
 	<?php 
 		require 'vendor/autoload.php';
 
@@ -39,13 +48,6 @@ $_SESSION['song'] = $NAME;
 		}
 		echo "<p>" . $songID . "</p>";
 	?> 
-	<div class = "post-body"> 
-		<form action="backend.php" method="POST" enctype="multipart/form-data">
-			<textarea class = "status" name = "post_Text" placeholder = "Write your post here!" rows="4" cols="50" maxlength = "300"autofocus></textarea> 
-			<label for="img">Attach image:</label>
-			<input type="file" id="img" name="img" accept="image/*">
-			<button class = 'btn btn-outline-info' type="submit" name="create_Post" value= 'Post' class="submit">Post</button></form>
-		</form>
-	</div>
+	
 
 </body></html>
