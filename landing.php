e845149e615c173407aadf2952f8a14a23b8d94e
brendan123 <brendanaszklar@gmail.com>
2021-11-16 23:05:41 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -120,7 +120,7 @@
                     echo "<div class='posts'>";
                     echo "<p class='puser'>". $row['user']."</p>";
                     if($_SESSION['isAdmin']==1){
-                    echo "<a href=backend.php?d='" . $row['id'] . "'" . ">" . "Delete post" . "</a>";
+                    echo '<a href=backend.php?d="' . $row['id'] . '"' . ">" . "Delete post" . "</a>";
                     }
                     echo '<img class="post_image" src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
                     echo "<p class='ptext'>". $row['text']."</p>";
