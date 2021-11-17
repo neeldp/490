cb49b63b160bc583cf1ca56994437858cfbabe99
neeldp <ndp69@njit.edu>
2021-11-17 17:33:03 -0500

    Update following.php

--- following.php
+++ following.php
@@ -12,11 +12,16 @@ require 'db_key.php';
     $row = mysqli_fetch_array($result);
 	$user_id = $row['id'];
 
-    $sql = $conn->query("SELECT distinct `user` FROM followers_table WHERE follower_id = '{$user_id}'");
+    $sql = $conn->query("SELECT distinct * FROM followers_table WHERE follower_id = '{$user_id}'");
     if($sql->num_rows > 0){
         while($row = mysqli_fetch_array($sql))
         {
             echo $row['user'];
+            echo '<form method="POST" action="backend.php"> <b>
+					<input type="hidden" name="id" value="'. $row['id'].'" /> </b>
+					<input type="hidden" name="name" value="'. $row['user'].'" />
+					<button class = "btn btn-outline-info" type="submit" name="unfollowButton" value= "following">Following</button>
+					</form>';
             echo '<br>';
         
         }
