e895a5310a7635e4375b798f653022cf2c986f27
jakeN <76239912+jtn24-NJIT@users.noreply.github.com>
2021-11-18 00:08:31 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -57,7 +57,7 @@
 
     <!-- Timeline Code --> 
     <div class = "timeline">
-	<h1> <b> My Timeline  </b></h1> 
+	<h1 class  = "my_Timeline_Orientation"> <b> My Timeline  </b></h1> 
         <?php
             $value = getenv("SPOTIFY_TOKEN");
             echo "<p>". $value."</p>";
