c6cef434eea9ed0c38e333aff1aa41729de214bc
brendan123 <brendanaszklar@gmail.com>
2021-12-06 15:07:15 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -20,7 +20,53 @@
             </div>
         </div>
         <!-- Nested row for non-featured blog posts-->
-        
+        <div class="row">
+            <div class="col-lg-6">
+                <!-- Blog post-->
+                <div class="card mb-4">
+                    <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                    <div class="card-body">
+                        <div class="small text-muted">January 1, 2021</div>
+                        <h2 class="card-title h4">Post Title</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+                        <a class="btn btn-primary" href="#!">Read more →</a>
+                    </div>
+                </div>
+                <!-- Blog post-->
+                <div class="card mb-4">
+                    <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                    <div class="card-body">
+                        <div class="small text-muted">January 1, 2021</div>
+                        <h2 class="card-title h4">Post Title</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+                        <a class="btn btn-primary" href="#!">Read more →</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <!-- Blog post-->
+                <div class="card mb-4">
+                    <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                    <div class="card-body">
+                        <div class="small text-muted">January 1, 2021</div>
+                        <h2 class="card-title h4">Post Title</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla.</p>
+                        <a class="btn btn-primary" href="#!">Read more →</a>
+                    </div>
+                </div>
+                <!-- Blog post-->
+                <div class="card mb-4">
+                    <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                    <div class="card-body">
+                        <div class="small text-muted">January 1, 2021</div>
+                        <h2 class="card-title h4">Post Title</h2>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis aliquid atque, nulla? Quos cum ex quis soluta, a laboriosam.</p>
+                        <a class="btn btn-primary" href="#!">Read more →</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <!-- Side widgets-->
     <div class="col-lg-4">
         <!-- Search widget-->
@@ -36,8 +82,8 @@
     <!-- Timeline Code --> 
     <div class='container'>
         <div class = 'row clearfix'>
-            <div class = "col-lg-12">
-                <div class = "card mb-12">
+            <div class = "col-lg-4">
+                <div class = "card mb-4">
                     <div class="card-header">You Might Like</div>
                 <?php
                 $user = $_SESSION['username'];
