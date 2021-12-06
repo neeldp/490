49237b7d7140751417e41ce00e03ff1ae7551bac
brendan123 <brendanaszklar@gmail.com>
2021-12-06 15:37:33 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -11,6 +11,90 @@
     <div class="col-lg-8">
         <!-- Featured blog post-->
         <div class="card mb-4">
+        <?php $value = getenv("SPOTIFY_TOKEN"); echo "<p>". $value."</p>"; ?>
+            
+            <?php
+                $user = $_SESSION['username'];
+                $conn = connect_db();
+                $result = $conn->query("SELECT id FROM users WHERE `username` = '{$user}'");
+                $row = mysqli_fetch_array($result);
+                $user_id = $row['id'];
+                
+                
+                $sql = $conn->query("SELECT distinct `user` FROM followers_table WHERE follower_id = '{$user_id}'");
+                $arr = array();
+                $counter = 0;
+                if($sql->num_rows > 0){
+                    while($row = mysqli_fetch_array($sql))
+                    {
+                        $arr[$counter] = $row['user'];
+                        $counter++;
+                    }
+                    $arr[$counter] = $user;
+                    //}
+                    $list = implode("' ,'", $arr);
+                    $sql_query = $conn->query("SELECT * FROM `posts` Where `user` IN ('{$list}') ORDER BY `time` DESC");
+                    //if($sql_query->num_rows > 0){
+                    while($row = mysqli_fetch_array($sql_query))
+                    {
+                        echo "<div class='card-header'>";
+                        echo "<p>". $row['user']."</p>";
+                        $name = $row['user'];
+                        if ($name != $user){
+                        
+                            $result = $conn->query("SELECT id FROM followers_table WHERE follower_id = '{$user_id}' AND `user` = '{$name}'");
+                            $record = mysqli_fetch_array($result);
+                            echo '<form method="POST" action="backend.php">
+                                    <input type="hidden" name="id" value="'. $record['id'].'" />
+                                    <input type="hidden" name="username" value="'. $row['user'].'" />
+                                    <button id = followerButton class = "btn btn-outline-info" type="submit" name="unfollowbtn" value= "unfollow">Following</button>
+                            </form>';
+                        }
+                
+                        $userP = $row['user'];
+                        $result = $conn->query("SELECT id FROM `followers_table` WHERE `user` = '{$userP}' AND `follower_id` = '{$user_id}'");
+                        $record = mysqli_fetch_array($result);
+                        $id = $row['id'];
+                        if($_SESSION['isAdmin']==1){
+                            echo '<form method="POST" action="backend.php">
+                            <input type="hidden" name="del" value="'. $id.'" />
+                            <button id = "delete-button"  class = "btn btn-outline-info" type="submit" name="deletebtn" value= "delete">Delete</button>
+                            </form>';
+                        }
+                        echo '<div class="card mb-4">';
+                        echo '<img class="card-img-post" src="data:image/jpeg;base64,'.base64_encode( $row['image'] ).'"/>';
+                        echo '<div class="card-body">';
+                        echo "<p class='card-text'>". $row['text']."</p>";
+                        echo "<iframe src='https://open.spotify.com/embed/track/". $row['spotID'] . "'" . 'width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>';
+                            
+                        $id = $row['id'];
+                        $sql = $conn->query("SELECT * FROM comments Where post_ID = '{$id}'");
+                        if($sql->num_rows > 0){
+                            while($r = mysqli_fetch_array($sql))
+                            {
+                                echo $r['date']."<br>";
+                                echo $r['name'].":";
+                                echo $r['text']."<br><br>";
+                            }
+                        }
+                        echo '<form method="POST" action="backend.php">';
+                        echo '<div class ="form-group">';
+                        echo '<label>Comment:</label>';
+                        echo '<input class= "form-control w-25" type="text" name="comment_timeline">';
+                        echo '<input type="hidden" name="post_ID" value="'. $row['id'].'" />';
+                        echo '</div>';
+                        echo '<button class = "btn btn-outline-info" type="submit" name="commentbtn" value= "post_Comment">Comment</button>';
+                        echo '</form>';
+                        echo "</div>";
+                        echo "</div>";
+                        echo "</div>";
+                        echo "<br><br>";
+                    }
+                }
+                $conn->close();     
+            ?>
+        </div>
+    </div>  
             <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>
             <div class="card-body">
                 <div class="small text-muted">January 1, 2021</div>
@@ -127,11 +211,8 @@
             </div>  
        
             <div class = "col-lg-8">
-            <h2> <b> My Timeline  </b></h2> 
-                <?php
-                    $value = getenv("SPOTIFY_TOKEN");
-                    echo "<p>". $value."</p>";
-                ?>
+            <h2><b>My Timeline</b></h2> 
+                <?php $value = getenv("SPOTIFY_TOKEN"); echo "<p>". $value."</p>"; ?>
             
                 <?php
                     $user = $_SESSION['username'];
