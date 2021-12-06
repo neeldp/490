25af7a69463859ebc2336a4406f2d3ab8b0186e2
brendan123 <brendanaszklar@gmail.com>
2021-12-06 16:32:13 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -85,7 +85,7 @@
             ?> 
         </div>
         <!-- Side widgets-->
-        <div class="col-lg-4" style="position:fixed;right:auto;">
+        <div class="col-lg-4" style="position:-webkit-sticky;position:sticky;">
             <!-- Search widget-->
             <br>
             <div class="card mb-4">
