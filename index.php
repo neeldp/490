6cea0a07204da1c3537837d7577c7e0469b39724
brendan123 <brendanaszklar@gmail.com>
2021-12-06 18:36:55 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,22 @@
 <?php require 'header.php'; require 'db_key.php';?>
+<style>
+    .index{
+        background:linear-gradient(#2C3E50,#000000)  
+    }
+</style>
 
-<body class = "nonScrollable" style="background:linear-gradient(#2C3E50,#000000)">
+<body class = "index"> 
 
-<div class="scene" >
-    <div class="card">
-        <div class="login_Style">
-            <div class = "form-container">
-                <form method = 'POST' action = 'backend.php' >
-                    <img class = "loginLogo" src="transparentTreble2.png" alt="Treble Logo"/>
-                    
-                    <!--<h1 class = "title"> Welcome to Treble! </h1> <br>-->
-                    <div class="form-group">
-                        <label>Username : </label><br>
-                        <input class= 'form-control w-25' type="text" name="username"><br>
+    <div class = "container">
+        <img class = "loginLogo" src="transparentTreble2.png" alt="Treble Logo"/>
+        <form method = 'POST' action = 'backend.php' > 
+            <div>
+                <input class= 'form-control w-25' type="text" name="username"><br>
+                <input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
+            </div> 
+            <br><button class = 'button-login' type="submit" name="login" value= 'login' class="submit">Login</button>
+        </form>	
+    </div>
 
-                        <label>Password :</label><br>
-                        <input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
-                    </div> 
-                    <br><button class = 'button-login' type="submit" name="login" value= 'login' class="submit">Login</button>
-                </form>	
-            </div>
-        </div>       
-    </div>   
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
