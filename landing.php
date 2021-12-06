505a537f4e7bf827f832c4d03e94f19cb66eefee
brendan123 <brendanaszklar@gmail.com>
2021-12-06 16:41:39 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -85,50 +85,52 @@
             ?> 
         </div>
         <!-- Side widgets-->
-        <div class="col-lg-4" style="position:-webkit-sticky;position:sticky;">
-            <!-- Search widget-->
-            <br>
-            <div class="card mb-4">
-                <div class="card-header">Search</div>
-                <div class="card-body">
-                    <div class="input-group">
-                        <input class="form-control" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
-                        <button class="btn btn-primary" id="button-search" type="button">Go!</button>
+        <div style='float:right;width=200px'>
+            <div class="col-lg-4" style="position:fixed;">
+                <!-- Search widget-->
+                <br>
+                <div class="card mb-4">
+                    <div class="card-header">Search</div>
+                    <div class="card-body">
+                        <div class="input-group">
+                            <input class="form-control" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
+                            <button class="btn btn-primary" id="button-search" type="button">Go!</button>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <div class = "card mb-4">
-                <div class="card-header">You Might Like</div>
-                    <?php
-                        $user = $_SESSION['username']; $conn = connect_db(); $result = $conn->query("SELECT id FROM users WHERE `username` = '{$user}'");
-                        $row = mysqli_fetch_array($result); $user_id = $row['id'];
-                    
-                        $sql = $conn->query("SELECT distinct `user` FROM followers_table WHERE follower_id = '{$user_id}'");
-                        $arr = array(); $counter = 0;
-                        if($sql->num_rows > 0){
-                            while($row = mysqli_fetch_array($sql)) {
-                                $arr[$counter] = $row['user'];
-                                $counter++;
-                            }
-                            $arr[$counter] = $user;
-                        } 
-                        $list = implode("' ,'", $arr);
-                        $sql_query = $conn->query("SELECT * FROM `users` Where `username` NOT IN ('{$list}') ORDER BY Rand() LIMIT 3 ");
-                        //AND NOT IN '{$user}'
-                        echo '<div class ="card-body">';
-                            if($sql_query->num_rows > 0){
-                                while($row = mysqli_fetch_array($sql_query)){
-                                    echo $row['username'];    
-                                    echo '<form method="POST" action="backend.php">
-                                    <input type="hidden" name="user" value="'. $row['username'].'" /> 
-                                    <button id = followerButton class = "btn btn-outline-info" type="submit" name="followbtn" value= "follower">+ Follow</button>
-                                    </form>';
-                                    echo '<br>';   
+                <div class = "card mb-4">
+                    <div class="card-header">You Might Like</div>
+                        <?php
+                            $user = $_SESSION['username']; $conn = connect_db(); $result = $conn->query("SELECT id FROM users WHERE `username` = '{$user}'");
+                            $row = mysqli_fetch_array($result); $user_id = $row['id'];
+                        
+                            $sql = $conn->query("SELECT distinct `user` FROM followers_table WHERE follower_id = '{$user_id}'");
+                            $arr = array(); $counter = 0;
+                            if($sql->num_rows > 0){
+                                while($row = mysqli_fetch_array($sql)) {
+                                    $arr[$counter] = $row['user'];
+                                    $counter++;
                                 }
-                            }
-                        echo '</div>';
-                    ?>
+                                $arr[$counter] = $user;
+                            } 
+                            $list = implode("' ,'", $arr);
+                            $sql_query = $conn->query("SELECT * FROM `users` Where `username` NOT IN ('{$list}') ORDER BY Rand() LIMIT 3 ");
+                            //AND NOT IN '{$user}'
+                            echo '<div class ="card-body">';
+                                if($sql_query->num_rows > 0){
+                                    while($row = mysqli_fetch_array($sql_query)){
+                                        echo $row['username'];    
+                                        echo '<form method="POST" action="backend.php">
+                                        <input type="hidden" name="user" value="'. $row['username'].'" /> 
+                                        <button id = followerButton class = "btn btn-outline-info" type="submit" name="followbtn" value= "follower">+ Follow</button>
+                                        </form>';
+                                        echo '<br>';   
+                                    }
+                                }
+                            echo '</div>';
+                      ?>
+                </div>  
             </div> 
         </div>            
     </div>
