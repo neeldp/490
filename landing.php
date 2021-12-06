6380471be7d91719dd0913f6c80d2c23686dc7ea
brendan123 <brendanaszklar@gmail.com>
2021-12-06 17:11:16 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -88,7 +88,7 @@
             ?> 
         </div>
         <!-- Side widgets-->
-        <div class="col-lg-2" >
+        <div class="col-lg-2" style='overflow-y: auto;' >
             <!-- Search widget-->
             <br>
             <div class="card mb-2">
