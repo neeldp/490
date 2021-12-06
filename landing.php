2c3db56f8c3f4b76b1fbdfbb2c0f361f95796019
neeldp <ndp69@njit.edu>
2021-12-06 17:22:35 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -8,7 +8,7 @@
 <div class="container">
     <div class="row">
     <!-- Blog entries-->
-        <div class="col-lg-6">
+        <div class="col-lg-6" style="float: right;">
             <!-- Featured blog post-->
             
             <?php $value = getenv("SPOTIFY_TOKEN"); echo "<p>". $value."</p>"; ?> 
@@ -91,7 +91,6 @@
         <div class="col-lg-2" style='overflow-y: auto;' >
             <!-- Search widget-->
             <br>
-            <div style="position:fixed">
             <div class="card mb-2">
                 <div class="card-header">Search</div>
                 <div class="card-body">
@@ -134,8 +133,7 @@
                         echo '</div>';
                     ?>
             </div> 
-        </div>  
-     </div>          
+        </div>            
     </div>
 </div>
     <!-- Timeline Code --> 
