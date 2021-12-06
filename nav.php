c7b90442ec168573c77e14112ac8a665b5e6c4ff
brendan123 <brendanaszklar@gmail.com>
2021-12-06 10:30:25 -0500

    Update nav.php

--- nav.php
+++ nav.php
@@ -2,21 +2,27 @@
 <header class="p-3 bg-dark text-white">
     <div class="container">
       <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+        <a href="landing.php" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
           <img  src="trebleNav.png"/>
         </a>
 
         <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Features</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">Pricing</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">FAQs</a></li>
-          <li><a href="#" class="nav-link px-2 text-white">About</a></li>
+          <li><a href="landing.php" class="nav-link px-2 text-secondary">Home</a></li>
+          <li><a href="create_Post.php" class="nav-link px-2 text-white">Create Post</a></li>
+          <li><a href="inbox.php" class="nav-link px-2 text-white">inbox.php</a></li>
+          <li><a href="profile.php" class="nav-link px-2 text-white">Profile</a></li>
+          <li><a href="index.php" class="nav-link px-2 text-white">Logout</a></li>
         </ul>
 
-        <form class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
-          <input type="search" class="form-control form-control-dark" placeholder="Search..." aria-label="Search">
-        </form>
+        <div class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+        <form method="POST" action='backend.php'>
+					<div class='form-group'>
+						<label>Search:</label>
+						<input class= 'form-control w-25' type="text" name="search">
+					</div>	
+					<sp><button type="submit" name="searchbtn" value= 'searchbtn' class="searchbtn">Search</button>
+				</form>
+        </div>
 
         <div class="text-end">
           <button type="button" class="btn btn-outline-light me-2">Login</button>
