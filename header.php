b62c09d0ed8200fefdceddc294c696d9c882fce0
brendan123 <brendanaszklar@gmail.com>
2021-12-06 10:03:40 -0500

    t

--- header.php
+++ header.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html>
 
-
-
 <head>
-    <title> Not Twitter </title>
+    <title> Treble </title>
     <meta charset = "utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="indexstyles.css" media="screen">
     <link rel="shortcut icon" href="#">
