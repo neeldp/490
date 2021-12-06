c6efc1fcdb0e558afea50ef795ca8c28c63bcfcc
brendan123 <brendanaszklar@gmail.com>
2021-12-06 12:26:53 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -90,7 +90,7 @@
                     echo '<button class = "btn btn-outline-info" type="submit" name="commentbtn" value= "post_Comment">Comment</button>';
                     echo '</form>';
                     echo "</div>";
-                    
+                    echo "</div>";
                     echo "<br><br>";
                 }
             }
@@ -141,7 +141,7 @@
             ?>
         </div>  
         </div>
-    </div></div></div>
+    </div></div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body></html> 
