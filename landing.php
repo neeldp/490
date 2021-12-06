0fda684050597c20b7aaed7991724f4e31c0a165
brendan123 <brendanaszklar@gmail.com>
2021-12-06 17:30:26 -0500

    Update landing.php

--- landing.php
+++ landing.php
@@ -95,7 +95,12 @@
             <div class="card mb-2">
                 <div class="card-header">Search</div>
                 <div class="card-body">
+                
                     <div class="input-group">
+                        <form method="POST" action='backend.php'>
+                        <label>Search:</label>
+					    <input class= 'form-control w-25' type="text" name="search">
+                        <button type="submit" name="searchbtn" value= 'searchbtn' class="searchbtn">Search</button>
                         <input class="form-control" type="text" name="search" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
                         <button class="btn btn-primary" id="button-search" type="submit" name='searchbtn'>Go!</button>
                     </div>
