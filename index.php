743b34f913d4c3a007378ce39598ad08763f688f
brendan123 <brendanaszklar@gmail.com>
2021-12-06 18:48:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,14 +11,13 @@
         margin: 0;
         background-repeat: no-repeat;
         background-attachment: fixed;
+        background:linear-gradient(#2C3E50,#212e3c,#161f28,#0f151b,#000000)  
     }
-    
 </style>
 
 <body class = "index"> 
 
-    <div class = "row">
-        <div class="col-4 d-flex justify-content-center text-center">
+    <div class = "container">
         <img class = "loginLogo" src="transparentTreble2.png" alt="Treble Logo"/>
         <form method = 'POST' action = 'backend.php' > 
             <div>
@@ -27,7 +26,6 @@
             </div> 
             <br><button class = 'button-login' type="submit" name="login" value= 'login' class="submit">Login</button>
         </form>	
-    </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
