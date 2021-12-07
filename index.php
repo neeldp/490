6973980d5df53906e16b726603837526fdc4d59a
brendan123 <brendanaszklar@gmail.com>
2021-12-07 06:40:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
     <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div class="card bg-dark text-white" style="border-radius: 1rem;">
+            <div class="card  text-white" style="border-radius: 1rem;">
             <div class="card-body p-5 text-center">
 
                 <div class="mb-md-5 mt-md-4 pb-5">
                 <img src="transparentTreble2.png" alt="Treble Logo"/> 
-                <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
-                <p class="text-white-50 mb-5">Please enter your login and password!</p>
+                <!--<h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                <p class="text-white-50 mb-5">Please enter your login and password!</p>-->
                 <form method="POST" action='backend.php'>
                     <div class="form-outline form-white mb-4">
                         <input type="text" id="typeEmailX" class="form-control form-control-lg" name="username"/>
