53888ff77053291c71f08a6bc75100a1b430fbcc
brendan123 <brendanaszklar@gmail.com>
2021-12-06 19:03:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -20,21 +20,23 @@
 							
     }
     .container{
-        float:center;
-        position:absolute
+        display:flex;
+        
     }
 </style>
 
 <body class = "index"> 
 
-    <div class="container">
+<div class="d-flex justify-content-center">
         <img class = "loginLogo" src="transparentTreble2.png" alt="Treble Logo"/>
+    <div class = "container">
         <form method = 'POST' action = 'backend.php' >    
             <input class= 'form-control w-25' type="text" name="username"><br>
             <input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
             <br><button class = 'button-login' type="submit" name="login" value= 'login' class="submit">Login</button>
         </form>	
     </div>
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
