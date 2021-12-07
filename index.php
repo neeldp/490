bb558d0f31e96da38b3ae040f9f443cef0a7e7fd
brendan123 <brendanaszklar@gmail.com>
2021-12-07 06:34:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php require 'header.php'; require 'db_key.php';?>
+<style>
+    .gradient-custom {
+        background: #2c3e50;
+        background: -webkit-linear-gradient(left, #2c3e50, #000000);
+
+    /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    background:         linear-gradient(to right, #2c3e50, #000000);
+    }
+</style>
     <section class="vh-100 gradient-custom">
     <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
@@ -21,9 +30,8 @@
                     <label class="form-label" for="typePasswordX" name="password">Password</label>
                 </div>
 
-                <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p>
 
-                <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+                <button class="btn btn-outline-light btn-lg px-5" type="submit" name="login" value="login">Login</button>
 
 
                 </div>
