9d10e28a74895d2aa33117c714a7899486b0d325
brendan123 <brendanaszklar@gmail.com>
2021-12-07 05:39:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -16,14 +16,12 @@
     }
     .lgForm {
         min-width: 500px;
-        position: absolute;
+        
         text-align: center;
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
         font-size: 2.5rem;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
         background: #2c3e50;
      
     }
