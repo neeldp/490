40cb7d306d4ad110f6b57f173d97552e0ebf97ba
brendan123 <brendanaszklar@gmail.com>
2021-12-07 06:17:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,37 +1,23 @@
 <?php require 'header.php'; require 'db_key.php';?>
-<style>   
-    body {
-       
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-        background: #2c3e50;
-        background: -webkit-linear-gradient(left, #2c3e50, #000000);
-        background:    -moz-linear-gradient(left, #2c3e50, #000000);
-        background:     -ms-linear-gradient(left, #2c3e50, #000000);
-        background:      -o-linear-gradient(left, #2c3e50, #000000);
-        background:         linear-gradient(to right, #2c3e50, #000000);
-                            
-    }
-  
-    
-</style>
 
-<body>   
-    
-<div class = "container">
-    <div class="row">
-        <div class="col-sm-4 col-sm-offset-4">
-            <div class="input-group">
-                <img src="transparentTreble2.png" alt="Treble Logo"/> 
-                <form method = 'POST' action = 'backend.php' >    
-                    <input class= 'form-control' type="text" name="username"><br>
-                    <input class= 'form-control' type="password" name="password" id="password" autocomplete="off">
-                    <br><button class = 'button-login' type="submit" name="login" value= 'login' class="submit">Login</button>
-                </form>	  
-            </div>
-        </div>    
-    </div>
-</div>
+<body class="text-center">
+    <form class="form-signin">
+      <img class="mb-4" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+      <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+      <label for="inputEmail" class="sr-only">Email address</label>
+      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+      <label for="inputPassword" class="sr-only">Password</label>
+      <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <div class="checkbox mb-3">
+        <label>
+          <input type="checkbox" value="remember-me"> Remember me
+        </label>
+      </div>
+      <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      <p class="mt-5 mb-3 text-muted">&copy; 2017-2018</p>
+    </form>
+  </body>
+</html>
 
  
         
