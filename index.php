5649784e7778864d680e4960a86a7b6024cd8a82
brendan123 <brendanaszklar@gmail.com>
2021-12-07 06:42:14 -0500

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <div class="container py-5 h-100">
         <div class="row d-flex justify-content-center align-items-center h-100">
         <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-            <div class="card  text-white" style="border-radius: 1rem;">
+            <div class="card bf-secondary text-white" style="border-radius: 1rem;">
             <div class="card-body p-5 text-center">
 
                 <div class="mb-md-5 mt-md-4 pb-5">
