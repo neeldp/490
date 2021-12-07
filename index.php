4076e808907d58b7e5aca3e46d26dfa015841d70
brendan123 <brendanaszklar@gmail.com>
2021-12-07 06:14:45 -0500

    Update index.php

--- index.php
+++ index.php
@@ -20,16 +20,20 @@
     
 <div class = "container">
     <div class="row">
-        <div class=col-lg-12>
-            <img src="transparentTreble2.png" alt="Treble Logo"/> 
-            <form method = 'POST' action = 'backend.php' >    
-                <input class= 'form-control' type="text" name="username"><br>
-                <input class= 'form-control' type="password" name="password" id="password" autocomplete="off">
-                <br><button class = 'button-login' type="submit" name="login" value= 'login' class="submit">Login</button>
-            </form>	  
-        </div>
+        <div class="col-sm-4 col-sm-offset-4">
+            <div class="input-group">
+                <img src="transparentTreble2.png" alt="Treble Logo"/> 
+                <form method = 'POST' action = 'backend.php' >    
+                    <input class= 'form-control' type="text" name="username"><br>
+                    <input class= 'form-control' type="password" name="password" id="password" autocomplete="off">
+                    <br><button class = 'button-login' type="submit" name="login" value= 'login' class="submit">Login</button>
+                </form>	  
+            </div>
+        </div>    
     </div>
 </div>
+
+ 
         
         
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
