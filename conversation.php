ce9ececbf18d0ab257ca7772c0c90bfea8ae4ce9
brendan123 <brendanaszklar@gmail.com>
2021-12-07 10:44:18 -0500

    Update conversation.php

--- conversation.php
+++ conversation.php
@@ -16,7 +16,7 @@ $_SESSION['get'] = $NAME;
     });
     </script>
     
-    <div class="card">
+    <div class="card mx-auto">
         <div class="card-body" id="fresh">
             <?php
                $conn = connect_db();
@@ -35,17 +35,17 @@ $_SESSION['get'] = $NAME;
              document.getElementbyId("fresh").scrollTop =  document.getElementbyId("fresh").scrollHeight;   
             </script>
         </div>
+    
+        <div class ="form-container message">
+            <form method='POST' action ='backend.php'>
+                <div class='form-group'>
+                    <label>Message:</label>
+                    <input class= 'form-control w-25' type="text" name="message">
+                </div>
+                <button class = 'btn btn-outline-info' type="submit" name="replyMsg" value= 'message' class="replyMsg">Message</button>        
+            </form>
+        </div> 
     </div>
-     
-    <div class ="form-container message">
-        <form method='POST' action ='backend.php'>
-            <div class='form-group'>
-                <label>Message:</label>
-                <input class= 'form-control w-25' type="text" name="message">
-            </div>
-            <button class = 'btn btn-outline-info' type="submit" name="replyMsg" value= 'message' class="replyMsg">Message</button>        
-        </form>
-    </div> 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
 </body>
