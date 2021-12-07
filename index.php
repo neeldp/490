827a0868c6c0da10fdc57189dd93af7f606667a8
brendan123 <brendanaszklar@gmail.com>
2021-12-07 05:25:56 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,7 @@
 <?php require 'header.php'; require 'db_key.php';?>
-<style>
-    
-    html {
-        height: 100%;
-    }
+<style>   
     body {
+        display:flex;
         height: 100%;
         margin: 0;
         background-repeat: no-repeat;
@@ -15,28 +12,26 @@
         background:     -ms-linear-gradient(left, #2c3e50, #000000);
         background:      -o-linear-gradient(left, #2c3e50, #000000);
         background:         linear-gradient(to right, #2c3e50, #000000);
-							
+                            
     }
-    .container{
-        margin:0,auto;
-        
+    form{
+        margin:auto;
     }
+
 </style>
 
 <body> 
-
-<div class="container">
-    <div class="row">
-        <div class = "col-4">        
-            <form method = 'POST' action = 'backend.php' >    
-                <img class = "mx-auto d-block" src="transparentTreble2.png" alt="Treble Logo"/>
-                <input class= 'form-control w-25' type="text" name="username"><br>
-                <input class= 'form-control w-25' type="password" name="password" id="password" autocomplete="off">
-                <br><button class = 'button-login' type="submit" name="login" value= 'login' class="submit">Login</button>
-            </form>	
-        </div>
-    </div>  
-</div>
-
+    <div class="container">
+        <div class="row">
+            <div class = "col-4">        
+                <form method = 'POST' action = 'backend.php' >    
+                    <img class = "mx-auto d-block" src="transparentTreble2.png" alt="Treble Logo"/>
+                    <input class= 'form-control' type="text" name="username"><br>
+                    <input class= 'form-control' type="password" name="password" id="password" autocomplete="off">
+                    <br><button class = 'button-login' type="submit" name="login" value= 'login' class="submit">Login</button>
+                </form>	
+            </div>
+        </div>  
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
