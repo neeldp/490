82579fac0b32ecacbc576c8da72f48f024d93fa5
brendan123 <brendanaszklar@gmail.com>
2021-12-07 23:27:46 -0500

    Update sendMessage.php

--- sendMessage.php
+++ sendMessage.php
@@ -3,6 +3,7 @@
 <!-- This file is used for creating a post for the CS 490 SEC. 003 Group 8 Simple Social Media Project -->
 
 <body>
+</br>
 <div class='container'>
 	<div class='row'> 
 		<div card ="col-lg-7">
@@ -25,8 +26,6 @@
                 </div>
             </div></div>
         </div>
-    </div>   
-
-    
+    </div>     
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
