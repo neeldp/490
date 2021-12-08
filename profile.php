2d89e0073387e1393d8b565af00fac716e007bf6
brendan123 <brendanaszklar@gmail.com>
2021-12-08 10:54:37 -0500

    Update profile.php

--- profile.php
+++ profile.php
@@ -115,56 +115,44 @@ require 'db_key.php';
              <!-- Search widget -->
             <br>
             <div style="position:fixed">
-             <!-- <div class="card mb-2">
-                <div class="card-header">Search</div>
-                <div class="card-body">
+            	<div class = "card mb-4">
+					<div class="card-header">Fans</div>
+					<script>
+						function following() {
+							window.location.replace("following.php");
+						}
 
-                    <div class="input-group">
-                        <form method="POST" action='backend.php'>
-                        <input class="form-control" type="text" name="search" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
-                        <button class="btn btn-primary" id="button-search" type="submit" name='searchbtn'>Go!</button>
-                    </div>
-                    </form>
-                </div>
-                </div> -->
+						function followers() {
+							window.location.replace("followers.php");
+						}
+					</script>
 
-             <div class = "card mb-4">
-                <div class="card-header">You Might Like</div>
-                <?php
-                        $user = $_SESSION['username']; 
-                        $conn = connect_db(); 
-                        $result = $conn->query("SELECT id FROM users WHERE `username` = '{$user}'");
-                        $row = mysqli_fetch_array($result); 
-                        $user_id = $row['id'];
-                    
-                        $sql = $conn->query("SELECT distinct `user` FROM followers_table WHERE follower_id = '{$user_id}'");
-                        $arr = array(); 
-                        $counter = 0;
-                        if($sql->num_rows > 0){
-                            while($row = mysqli_fetch_array($sql)) {
-                                $arr[$counter] = $row['user'];
-                                $counter++;
-                            }
-                            //$arr[$counter] = $user;
-                            //var_dump($arr);
-                        } 
-                        $list = implode("' ,'", $arr);
-                        $sql_query = $conn->query("SELECT * FROM `users` Where `username` NOT IN ('{$list}') and username != '{$user}' ORDER BY Rand() LIMIT 3 ");
-                        echo '<div class ="card-body">';
-                            if($sql_query->num_rows > 0){
-                                while($row = mysqli_fetch_array($sql_query)){
-                                    echo $row['username'];    
-                                    echo '<form method="POST" action="backend.php">
-                                    <input type="hidden" name="user" value="'. $row['username'].'" /> 
-                                    <button id = followerButton class = "btn btn-outline-info" type="submit" name="followbtn" value= "follower">+ Follow</button>
-                                    </form>';
-                                    echo '<br>';   
-                                }
-                            }
-                        echo '</div>';
-                    ?>
-            </div> 
-         </div>
+					<?php
+
+						$conn = connect_db();
+						$user = $_SESSION['username'];
+						$result = $conn->query("SELECT id FROM users WHERE `username` = '{$user}'");
+						$row = mysqli_fetch_array($result);
+						$user_id = $row['id'];
+
+						$sql_query = $conn->query("SELECT COUNT(*) FROM followers_table WHERE `follower_id` = '{$user_id}'");
+						$row = mysqli_fetch_array($sql_query);
+						//echo "$row[0]";
+						//echo '<a href="following.php">following</a>';
+						//echo "$row[0] ";
+						echo '<label onclick="following()" class="followingPointer">'. $row[0].' Following</label>'; /* WHY DOESN'T THE ID WORK/MAKES THE FUNCTION STOP WORKING?? */
+						//echo '<label onclick="following()">'. $row[0].' Following</label>';
+						//echo "\t\t\t\t";
+						
+
+						$sql = $conn->query("SELECT COUNT(*) FROM followers_table WHERE `user` = '{$user}'");
+						$row = mysqli_fetch_array($sql);
+						//echo "$row[0] ";
+						echo '<label onclick="followers()" class="followerPointer">'. $row[0].' Followers</label>';
+
+					?>
+				</div> 
+			</div>
 			</div>
 		</div>
 	</body>
