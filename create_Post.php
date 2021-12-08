47c86024b1614547f34bece8431edd85f0f9adbf
brendan123 <brendanaszklar@gmail.com>
2021-12-07 23:16:08 -0500

    Update create_Post.php

--- create_Post.php
+++ create_Post.php
@@ -10,57 +10,61 @@ $_SESSION['song'] = $NAME;
 <div class='container'>
 	<div class='row'> 
 		<div card ="col-lg-7">
-			<h1> Create A Post </h1>
-			<div class='card mb-4'>
-				<div class='card-body'> 
-					<form action="backend.php" method="POST" enctype="multipart/form-data">   	
-						<div class="form-group">
-							<label for="spotSearch" card="card-title">Search for a Song on Spotify:</label>
-							<input class="form-control" id="spotSearch" type="text" name="searchSong">
-						</div>	
-						<sp> <button type="submit" name="searchbtnsong" value="searchbtn" class="searchbtnsong">Search</button> <br> <br>
-					</form>
-					
-					<?php
-						if($_GET){ 
-							echo"<div class ='card mx-auto'>";
-							echo	"<div class ='card-body'>";
-								echo "<p class='card-title'> Spotify Search Results </p>";
-								require 'vendor/autoload.php';
+		<div class='card mb-4'>
+			<div class='card-body'> 
+				<h1> Create A Post </h1>
+				<div class='card mb-4'>
+					<div class='card-body'> 
+						<form action="backend.php" method="POST" enctype="multipart/form-data">   	
+							<div class="form-group">
+								<label for="spotSearch" card="card-title">Search for a Song on Spotify:</label>
+								<input class="form-control" id="spotSearch" type="text" name="searchSong">
+							</div>	
+							<sp> <button type="submit" name="searchbtnsong" value="searchbtn" class="searchbtnsong">Search</button> <br> <br>
+						</form>
+						
+						<?php
+							if($_GET){ 
+								echo"<div class ='card mx-auto'>";
+								echo	"<div class ='card-body'>";
+									echo "<p class='card-title'> Spotify Search Results </p>";
+									require 'vendor/autoload.php';
 
-								$session = new SpotifyWebAPI\Session(
-									'b7d9baca79b6424597551d19d5fd02cf',
-									'6b988bb0c7ae4cf58013ff29e6ce5a26',
-									'https://login490.herokuapp.com/redir.php'
-								);
-								
-								$api = new SpotifyWebAPI\SpotifyWebAPI();
-								
-								$session->requestCredentialsToken();
-								$accessToken = $session->getAccessToken();
-								
-								// Set the code on the API wrapper
-								$api->setAccessToken($accessToken);
-								//echo "<p>" . $accessToken . "</p>";
-								
-								$res = $api->search($_SESSION["song"], ['track']);
-								foreach($res->tracks->items as $track){
-									echo "<a href='songredir.php?id=". $track->id . "'>" . $track->name . $track->artist . "</a>" . '<br>';
-								}
-							echo	"</div>";
-						echo "</div>";	
-						}
-					?> 
-					
-					<form action="backend.php" method="POST" enctype="multipart/form-data"> 
-						<div class='form-group'>
-							<label for="img">Attach image:</label><br>
-							<input type="file" id="img" name="img" accept="image/*">  <br><br>
-					
-							<textarea class="form-control" name="post_Text" placeholder="Write your post here!"></textarea> 
-							<button class="btn btn-outline-info" type="submit" name="create_Post" value="Post">Post</button> <br>
+									$session = new SpotifyWebAPI\Session(
+										'b7d9baca79b6424597551d19d5fd02cf',
+										'6b988bb0c7ae4cf58013ff29e6ce5a26',
+										'https://login490.herokuapp.com/redir.php'
+									);
+									
+									$api = new SpotifyWebAPI\SpotifyWebAPI();
+									
+									$session->requestCredentialsToken();
+									$accessToken = $session->getAccessToken();
+									
+									// Set the code on the API wrapper
+									$api->setAccessToken($accessToken);
+									//echo "<p>" . $accessToken . "</p>";
+									
+									$res = $api->search($_SESSION["song"], ['track']);
+									foreach($res->tracks->items as $track){
+										echo "<a href='songredir.php?id=". $track->id . "'>" . $track->name . $track->artist . "</a>" . '<br>';
+									}
+								echo	"</div>";
+							echo "</div>";	
+							}
+						?> 
+						
+						<form action="backend.php" method="POST" enctype="multipart/form-data"> 
+							<div class='form-group'>
+								<label for="img">Attach image:</label><br>
+								<input type="file" id="img" name="img" accept="image/*">  <br><br>
+						
+								<textarea class="form-control" name="post_Text" placeholder="Write your post here!"></textarea> 
+								<button class="btn btn-outline-info" type="submit" name="create_Post" value="Post">Post</button> <br>
+							</div>
+						</form>
 						</div>
-					</form>
+					</div>
 				</div>
 			</div>
 		</div>
