b3a76dc54b9f93dc6f01aff24cfe56efee159e00
brendan123 <brendanaszklar@gmail.com>
2021-12-09 10:05:38 -0500

    following followers

--- followers.php
+++ followers.php
@@ -4,37 +4,51 @@ require 'nav.php';
 require 'db_key.php';
 ?>
 
-<?php
+<div class='container'>
+	<div class='row'> 
+		<div card ="col-lg-7">
+			<br>
+			<div class='card mb-4'>
+				<div class='card-body'>
+                    <h1> Followers </h1> 
+                    <a href="profile.php" class="buttonClass">Profile</a>
+                    <ul class='list-group'>
+                        <?php
 
-    $user = $_SESSION['username'];
-    $conn = connect_db();
+                            $user = $_SESSION['username'];
+                            $conn = connect_db();
 
-    $sql = $conn->query("SELECT distinct `follower_id` FROM followers_table WHERE `user` = '{$user}'");
-    $counter = 0;
-    $arr = array();
-    if($sql->num_rows > 0){
-        while($row = mysqli_fetch_array($sql))
-        {
-            $id = $row['follower_id'];
-            //echo $id;
-            //$result = $conn->query("SELECT `username` FROM `users` WHERE `id` = '{$id}'");
-            //$record = mysqli_fetch_array($result);
-            //$follower = $record[0];
-            $arr[$counter] = $id;
-            $counter++;
+                            $sql = $conn->query("SELECT distinct `follower_id` FROM followers_table WHERE `user` = '{$user}'");
+                            $counter = 0;
+                            $arr = array();
+                            if($sql->num_rows > 0){
+                                while($row = mysqli_fetch_array($sql))
+                                {
+                                    $id = $row['follower_id'];
+                                    //echo $id;
+                                    //$result = $conn->query("SELECT `username` FROM `users` WHERE `id` = '{$id}'");
+                                    //$record = mysqli_fetch_array($result);
+                                    //$follower = $record[0];
+                                    $arr[$counter] = $id;
+                                    $counter++;
 
-            //echo "$record";
-            //echo '<br>'; 
-        }
-        
-        $list = implode("' ,'", $arr);
-        $sql_query = $conn->query("SELECT `username` FROM `users` Where `id` IN ('{$list}')");
-        while($row = mysqli_fetch_array($sql_query))
-        {
-            echo $row['username']."<br>";
+                                    //echo "$record";
+                                    //echo '<br>'; 
+                                }
+                                
+                                $list = implode("' ,'", $arr);
+                                $sql_query = $conn->query("SELECT `username` FROM `users` Where `id` IN ('{$list}')");
+                                while($row = mysqli_fetch_array($sql_query))
+                                {
+                                    echo "<li class='list-group-item'>". $row['username']."</li>";
 
-        }
+                                }
 
-    }
+                            }
 
-?>
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
--- following.php
+++ following.php
@@ -3,28 +3,41 @@ require 'header.php';
 require 'nav.php';
 require 'db_key.php';
 ?>
+<div class='container'>
+	<div class='row'> 
+		<div card ="col-lg-7">
+			<br>
+			<div class='card mb-4'>
+				<div class='card-body'>
+                    <h1> Following </h1> 
+                    <a href="profile.php" class="buttonClass">Profile</a>
+                    <ul class='list-group'>
+                        <?php
 
-<?php
+                            $user = $_SESSION['username'];
+                            $conn = connect_db();		
+                            $result = $conn->query("SELECT id FROM users WHERE `username` = '{$user}'");
+                            $row = mysqli_fetch_array($result);
+                            $user_id = $row['id'];
 
-    $user = $_SESSION['username'];
-    $conn = connect_db();		
-    $result = $conn->query("SELECT id FROM users WHERE `username` = '{$user}'");
-    $row = mysqli_fetch_array($result);
-	$user_id = $row['id'];
+                            $sql = $conn->query("SELECT distinct * FROM followers_table WHERE follower_id = '{$user_id}'");
+                            if($sql->num_rows > 0){
+                                while($row = mysqli_fetch_array($sql))
+                                {
+                                    echo $row['user'];
+                                    echo '<form method="POST" action="backend.php"> <b>
+                                            <input type="hidden" name="id" value="'. $row['id'].'" /> </b>
+                                            <input type="hidden" name="name" value="'. $row['user'].'" />
+                                            <button class = "btn btn-outline-info" type="submit" name="unfollowButton" value= "following">Unfollow</button>
+                                            </form>';
+                                    echo '<br>';
+                                
+                                }
+                            }
 
-    $sql = $conn->query("SELECT distinct * FROM followers_table WHERE follower_id = '{$user_id}'");
-    if($sql->num_rows > 0){
-        while($row = mysqli_fetch_array($sql))
-        {
-            echo $row['user'];
-            echo '<form method="POST" action="backend.php"> <b>
-					<input type="hidden" name="id" value="'. $row['id'].'" /> </b>
-					<input type="hidden" name="name" value="'. $row['user'].'" />
-					<button class = "btn btn-outline-info" type="submit" name="unfollowButton" value= "following">Unfollow</button>
-					</form>';
-            echo '<br>';
-        
-        }
-    }
-
-?>
+                        ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
